c2c3feb5c9fbe44255fb60c7b469f21dd36703cf
23rd <23rd@vivaldi.net>
2018-10-16 23:42:05 +0300

    Replace string 'Connecting to proxy' to '...'.

--- TMessagesProj/src/main/java/org/telegram/ui/ActionBar/ActionBar.java
+++ TMessagesProj/src/main/java/org/telegram/ui/ActionBar/ActionBar.java
@@ -871,6 +871,9 @@ public class ActionBar extends FrameLayout {
             return;
         }
         CharSequence textToSet = title != null ? LocaleController.getString(title, titleId) : lastTitle;
+        if (titleId == org.telegram.messenger.R.string.ConnectingToProxy) {
+            textToSet = "...";
+        }
         if (textToSet != null && titleTextView == null) {
             createTitleTextView();
         }
