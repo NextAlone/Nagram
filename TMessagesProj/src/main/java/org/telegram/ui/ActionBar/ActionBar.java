2a14a0371bee17d93da85fe91a00bf277ec9f02b
23rd <23rd@vivaldi.net>
2018-10-16 23:42:05 +0300

    Replaced string 'Connecting to proxy' to '...'.

--- TMessagesProj/src/main/java/org/telegram/ui/ActionBar/ActionBar.java
+++ TMessagesProj/src/main/java/org/telegram/ui/ActionBar/ActionBar.java
@@ -1167,6 +1167,9 @@ public class ActionBar extends FrameLayout {
                 ellipsize = true;
             }
         }
+        if (titleId == R.string.ConnectingToProxy) {
+            textToSet = "...";
+        }
         titleOverlayShown = title != null;
         if ((textToSet != null && titleTextView[0] == null) || getMeasuredWidth() == 0 || (titleTextView[0] != null && titleTextView[0].getVisibility() != View.VISIBLE)) {
             createTitleTextView(0);
