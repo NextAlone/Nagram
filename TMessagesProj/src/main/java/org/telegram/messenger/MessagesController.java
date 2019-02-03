cd63757d1d2c3566a327e2bb7508e3eba572c50a
23rd <23rd@vivaldi.net>
2019-02-03 22:50:44 +0300

    Added setting to raise all unmuted messages above muted messages.

--- TMessagesProj/src/main/java/org/telegram/messenger/MessagesController.java
+++ TMessagesProj/src/main/java/org/telegram/messenger/MessagesController.java
@@ -617,6 +617,8 @@ public class MessagesController extends BaseController implements NotificationCe
     };
 
     private Comparator<TLRPC.Dialog> dialogComparator = (dialog1, dialog2) -> {
+        boolean unmutedPrimary = getGlobalMainSettings().getBoolean("unmutedOnTop", false);
+
         if (dialog1 instanceof TLRPC.TL_dialogFolder && !(dialog2 instanceof TLRPC.TL_dialogFolder)) {
             return -1;
         } else if (!(dialog1 instanceof TLRPC.TL_dialogFolder) && dialog2 instanceof TLRPC.TL_dialogFolder) {
@@ -637,6 +639,31 @@ public class MessagesController extends BaseController implements NotificationCe
         MediaDataController mediaDataController = getMediaDataController();
         long date1 = DialogObject.getLastMessageOrDraftDate(dialog1, mediaDataController.getDraft(dialog1.id, 0));
         long date2 = DialogObject.getLastMessageOrDraftDate(dialog2, mediaDataController.getDraft(dialog2.id, 0));
+
+        if (unmutedPrimary) {
+            boolean mute1 = isDialogMuted(dialog1.id);
+            boolean mute2 = isDialogMuted(dialog2.id);
+            boolean unread1 = dialog1.unread_count > 0;
+            boolean unread2 = dialog2.unread_count > 0;
+
+            boolean newMention1 = unread1 && !mute1;
+            boolean newMention2 = unread2 && !mute2;
+
+            if (!newMention1 && newMention2) {
+                return 1;
+            } else if (newMention1 && !newMention2) {
+                return -1;
+            } else if (newMention1 && newMention2) {
+                if (date1 < date2) {
+                    return 1;
+                } else if (date1 > date2) {
+                    return -1;
+                } else {
+                    return 0;
+                }
+            }
+        }
+
         if (date1 < date2) {
             return 1;
         } else if (date1 > date2) {
@@ -7046,6 +7073,7 @@ public class MessagesController extends BaseController implements NotificationCe
                     }
                 }));
             }
+            sortDialogs(null);
         }
         if (!getUserConfig().notificationsSignUpSettingsLoaded) {
             loadSignUpNotificationsSettings();
@@ -8071,6 +8099,7 @@ public class MessagesController extends BaseController implements NotificationCe
         if (updated) {
             getNotificationCenter().postNotificationName(NotificationCenter.notificationsSettingsUpdated);
         }
+        sortDialogs(null);
     }
 
     private void applyDialogsNotificationsSettings(ArrayList<TLRPC.Dialog> dialogs) {
@@ -8107,6 +8136,7 @@ public class MessagesController extends BaseController implements NotificationCe
         if (editor != null) {
             editor.commit();
         }
+        sortDialogs(null);
     }
 
     public void reloadMentionsCountForChannel(TLRPC.InputPeer peer, long taskId) {
@@ -13267,6 +13297,7 @@ public class MessagesController extends BaseController implements NotificationCe
                             }
                             getMessagesStorage().updateMutedDialogsFiltersCounters();
                         }
+                        sortDialogs(null);
                     } else if (baseUpdate instanceof TLRPC.TL_updateChannel) {
                         TLRPC.TL_updateChannel update = (TLRPC.TL_updateChannel) baseUpdate;
                         TLRPC.Dialog dialog = dialogs_dict.get(-update.channel_id);
