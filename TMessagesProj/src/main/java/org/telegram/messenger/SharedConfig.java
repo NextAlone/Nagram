14ea1d29e477068788550ca0c71f85769d77346d
23rd <23rd@vivaldi.net>
2019-02-03 22:21:24 +0300

    Added option to disable sending sticker info in photos.

--- TMessagesProj/src/main/java/org/telegram/messenger/SharedConfig.java
+++ TMessagesProj/src/main/java/org/telegram/messenger/SharedConfig.java
@@ -116,6 +116,7 @@ public class SharedConfig {
     public static int distanceSystemType;
 
     public static boolean hideTitleDialog = false;
+    public static boolean hasSticker = false;;
 
     static {
         loadConfig();
@@ -294,6 +295,7 @@ public class SharedConfig {
             showNotificationsForAllAccounts = preferences.getBoolean("AllAccounts", true);
 
             hideTitleDialog = preferences.getBoolean("hideTitle", false);
+            hasSticker = preferences.getBoolean("photoHasSticker", false);
 
             configLoaded = true;
         }
@@ -780,6 +782,14 @@ public class SharedConfig {
         editor.putInt("distanceSystemType", distanceSystemType);
         editor.commit();
         LocaleController.resetImperialSystemType();
+    }
+
+    public static void toggleHasSticker() {
+        hasSticker = !hasSticker;
+        SharedPreferences preferences = MessagesController.getGlobalMainSettings();
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putBoolean("photoHasSticker", hasSticker);
+        editor.commit();
     }
 
     public static void loadProxyList() {
