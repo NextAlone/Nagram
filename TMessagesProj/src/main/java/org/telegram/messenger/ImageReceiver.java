b74504319fa366ed1977b0eb9db00c08885a39f8
23rd <23rd@vivaldi.net>
2019-02-03 22:03:39 +0300

    New Fork Settings section.
     - Square Avatars option.

--- TMessagesProj/src/main/java/org/telegram/messenger/ImageReceiver.java
+++ TMessagesProj/src/main/java/org/telegram/messenger/ImageReceiver.java
@@ -28,6 +28,8 @@ import org.telegram.tgnet.TLRPC;
 import org.telegram.ui.Components.AnimatedFileDrawable;
 import org.telegram.ui.Components.RecyclableDrawable;
 
+import android.content.SharedPreferences;
+
 public class ImageReceiver implements NotificationCenter.NotificationCenterDelegate {
 
     public interface ImageReceiverDelegate {
@@ -1300,7 +1302,13 @@ public class ImageReceiver implements NotificationCenter.NotificationCenterDeleg
     }
 
     public void setRoundRadius(int value) {
-        roundRadius = value;
+        SharedPreferences preferences = MessagesController.getGlobalMainSettings();
+        final boolean squareAvatars = preferences.getBoolean("squareAvatars", true);
+        if (!squareAvatars) {
+            roundRadius = value;
+        } else {
+            roundRadius = 0;
+        }
     }
 
     public void setCurrentAccount(int value) {
