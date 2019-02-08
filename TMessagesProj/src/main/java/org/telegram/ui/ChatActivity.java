d6dd2d6f0e31703120932179ce27809e4a4b2b95
23rd <23rd@vivaldi.net>
2019-02-08 21:16:48 +0300

    Added 'Go to First Message' item to dialog context menu.

--- TMessagesProj/src/main/java/org/telegram/ui/ChatActivity.java
+++ TMessagesProj/src/main/java/org/telegram/ui/ChatActivity.java
@@ -1300,6 +1300,7 @@ ChatActivity extends BaseFragment implements NotificationCenter.NotificationCent
     private final static int call = 32;
     private final static int video_call = 33;
     private final static int hideTitle = 34;
+    private final static int goToFirstMessage = 35;
 
     private final static int attach_photo = 0;
     private final static int attach_gallery = 1;
@@ -2482,6 +2483,10 @@ ChatActivity extends BaseFragment implements NotificationCenter.NotificationCent
 
                     SharedPreferences preferences = MessagesController.getGlobalMainSettings();
                     preferences.edit().putBoolean("hideTitle", SharedConfig.hideTitleDialog).commit();
+                } else if (id == goToFirstMessage) {
+                    // This is timestamp of launch date of the Telegram.
+                    // August 2013.
+                    jumpToDate(1375350800);
                 }
             }
         });
@@ -2936,6 +2941,8 @@ ChatActivity extends BaseFragment implements NotificationCenter.NotificationCent
                 hideTitleString = LocaleController.getString("HideTitle", R.string.HideTitle);
             }
             hideTitleItem = headerItem.addSubItem(hideTitle, R.drawable.hide_title, hideTitleString, themeDelegate);
+
+            headerItem.addSubItem(goToFirstMessage, R.drawable.to_first, LocaleController.getString("GoToFirstMessage", R.string.GoToFirstMessage), themeDelegate);
 
             if (currentUser != null) {
                 headerItem.addSubItem(call, R.drawable.msg_callback, LocaleController.getString("Call", R.string.Call), themeDelegate);
