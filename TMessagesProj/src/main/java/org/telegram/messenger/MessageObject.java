749ce3ff860cef96d073a22314c908ea3fef91e0
23rd <23rd@vivaldi.net>
2019-02-08 21:31:20 +0300

    Added Share Button to messages with media in dialogs with users.

--- TMessagesProj/src/main/java/org/telegram/messenger/MessageObject.java
+++ TMessagesProj/src/main/java/org/telegram/messenger/MessageObject.java
@@ -3753,7 +3753,9 @@ public class MessageObject {
                 return false;
             }
             TLRPC.User user = MessagesController.getInstance(currentAccount).getUser(messageOwner.from_id.user_id);
-            if (user != null && user.bot) {
+            if (user != null && !DialogObject.isSecretDialogId(getDialogId())
+            //  && user.bot
+                && messageOwner.from_id.user_id != UserConfig.getInstance(currentAccount).getClientUserId()) {
                 return true;
             }
             if (!isOut()) {
