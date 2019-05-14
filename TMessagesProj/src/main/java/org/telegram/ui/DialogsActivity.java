05db8793193f8d965fde9b9ffb458a27bfb99cfa
23rd <23rd@vivaldi.net>
2019-05-14 19:43:17 +0300

    Removed useless "New message" floating button.

--- TMessagesProj/src/main/java/org/telegram/ui/DialogsActivity.java
+++ TMessagesProj/src/main/java/org/telegram/ui/DialogsActivity.java
@@ -3304,27 +3304,27 @@ public class DialogsActivity extends BaseFragment implements NotificationCenter.
 
         floatingButtonContainer = new FrameLayout(context);
         floatingButtonContainer.setVisibility(onlySelect && initialDialogsType != 10 || folderId != 0 ? View.GONE : View.VISIBLE);
-        contentView.addView(floatingButtonContainer, LayoutHelper.createFrame((Build.VERSION.SDK_INT >= 21 ? 56 : 60), (Build.VERSION.SDK_INT >= 21 ? 56 : 60), (LocaleController.isRTL ? Gravity.LEFT : Gravity.RIGHT) | Gravity.BOTTOM, LocaleController.isRTL ? 14 : 0, 0, LocaleController.isRTL ? 0 : 14, 14));
-        floatingButtonContainer.setOnClickListener(v -> {
-            if (parentLayout != null && parentLayout.isInPreviewMode()) {
-                finishPreviewFragment();
-                return;
-            }
-            if (initialDialogsType == 10) {
-                if (delegate == null || selectedDialogs.isEmpty()) {
-                    return;
-                }
-                delegate.didSelectDialogs(DialogsActivity.this, selectedDialogs, null, false);
-            } else {
-                if (floatingButton.getVisibility() != View.VISIBLE) {
-                    return;
-                }
+        // contentView.addView(floatingButtonContainer, LayoutHelper.createFrame((Build.VERSION.SDK_INT >= 21 ? 56 : 60), (Build.VERSION.SDK_INT >= 21 ? 56 : 60), (LocaleController.isRTL ? Gravity.LEFT : Gravity.RIGHT) | Gravity.BOTTOM, LocaleController.isRTL ? 14 : 0, 0, LocaleController.isRTL ? 0 : 14, 14));
+        // floatingButtonContainer.setOnClickListener(v -> {
+        //     if (parentLayout != null && parentLayout.isInPreviewMode()) {
+        //         finishPreviewFragment();
+        //         return;
+        //     }
+        //     if (initialDialogsType == 10) {
+        //         if (delegate == null || selectedDialogs.isEmpty()) {
+        //             return;
+        //         }
+        //         delegate.didSelectDialogs(DialogsActivity.this, selectedDialogs, null, false);
+        //     } else {
+        //         if (floatingButton.getVisibility() != View.VISIBLE) {
+        //             return;
+        //         }
 
-                Bundle args = new Bundle();
-                args.putBoolean("destroyAfterSelect", true);
-                presentFragment(new ContactsActivity(args));
-            }
-        });
+        //         Bundle args = new Bundle();
+        //         args.putBoolean("destroyAfterSelect", true);
+        //         presentFragment(new ContactsActivity(args));
+        //     }
+        // });
 
         floatingButton = new RLottieImageView(context);
         floatingButton.setScaleType(ImageView.ScaleType.CENTER);
