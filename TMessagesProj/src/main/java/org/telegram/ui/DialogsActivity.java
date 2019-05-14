4bb94776b60ed635d36d7658e94b5632af3110f2
23rd <23rd@vivaldi.net>
2019-05-14 19:43:17 +0300

    Removed useless "New message" floating button.

--- TMessagesProj/src/main/java/org/telegram/ui/DialogsActivity.java
+++ TMessagesProj/src/main/java/org/telegram/ui/DialogsActivity.java
@@ -3132,19 +3132,19 @@ public class DialogsActivity extends BaseFragment implements NotificationCenter.
 
         floatingButtonContainer = new FrameLayout(context);
         floatingButtonContainer.setVisibility(onlySelect && initialDialogsType != 10 || folderId != 0 ? View.GONE : View.VISIBLE);
-        contentView.addView(floatingButtonContainer, LayoutHelper.createFrame((Build.VERSION.SDK_INT >= 21 ? 56 : 60) + 20, (Build.VERSION.SDK_INT >= 21 ? 56 : 60) + 20, (LocaleController.isRTL ? Gravity.LEFT : Gravity.RIGHT) | Gravity.BOTTOM, LocaleController.isRTL ? 4 : 0, 0, LocaleController.isRTL ? 0 : 4, 0));
-        floatingButtonContainer.setOnClickListener(v -> {
-            if (initialDialogsType == 10) {
-                if (delegate == null || selectedDialogs.isEmpty()) {
-                    return;
-                }
-                delegate.didSelectDialogs(DialogsActivity.this, selectedDialogs, null, false);
-            } else {
-                Bundle args = new Bundle();
-                args.putBoolean("destroyAfterSelect", true);
-                presentFragment(new ContactsActivity(args));
-            }
-        });
+        // contentView.addView(floatingButtonContainer, LayoutHelper.createFrame((Build.VERSION.SDK_INT >= 21 ? 56 : 60) + 20, (Build.VERSION.SDK_INT >= 21 ? 56 : 60) + 20, (LocaleController.isRTL ? Gravity.LEFT : Gravity.RIGHT) | Gravity.BOTTOM, LocaleController.isRTL ? 4 : 0, 0, LocaleController.isRTL ? 0 : 4, 0));
+        // floatingButtonContainer.setOnClickListener(v -> {
+        //     if (initialDialogsType == 10) {
+        //         if (delegate == null || selectedDialogs.isEmpty()) {
+        //             return;
+        //         }
+        //         delegate.didSelectDialogs(DialogsActivity.this, selectedDialogs, null, false);
+        //     } else {
+        //         Bundle args = new Bundle();
+        //         args.putBoolean("destroyAfterSelect", true);
+        //         presentFragment(new ContactsActivity(args));
+        //     }
+        // });
 
         floatingButton = new RLottieImageView(context);
         floatingButton.setScaleType(ImageView.ScaleType.CENTER);
