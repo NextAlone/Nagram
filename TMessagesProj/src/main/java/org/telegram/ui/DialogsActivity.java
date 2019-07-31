54e608f875f56b17069c9776c17d86bc4a4dae96
23rd <23rd@vivaldi.net>
2019-07-31 11:24:58 +0300

    Added ability to see profile info from dialogs list via context menu.

--- TMessagesProj/src/main/java/org/telegram/ui/DialogsActivity.java
+++ TMessagesProj/src/main/java/org/telegram/ui/DialogsActivity.java
@@ -131,6 +131,8 @@ import org.telegram.ui.Components.SizeNotifierFrameLayout;
 import org.telegram.ui.Components.StickersAlert;
 import org.telegram.ui.Components.UndoView;
 
+import org.telegram.ui.ProfileActivity;
+
 import java.util.ArrayList;
 
 public class DialogsActivity extends BaseFragment implements NotificationCenter.NotificationCenterDelegate {
@@ -174,6 +176,7 @@ public class DialogsActivity extends BaseFragment implements NotificationCenter.
     private ActionBarMenuSubItem clearItem;
     private ActionBarMenuSubItem readItem;
     private ActionBarMenuSubItem muteItem;
+    private ActionBarMenuSubItem profileInfoItem;
 
     private float additionalFloatingTranslation;
 
@@ -244,6 +247,7 @@ public class DialogsActivity extends BaseFragment implements NotificationCenter.
     private final static int clear = 103;
     private final static int mute = 104;
     private final static int archive = 105;
+    private final static int profileInfo = 110;
 
     private boolean allowScrollToHiddenView;
     private boolean scrollingManually;
@@ -927,7 +931,7 @@ public class DialogsActivity extends BaseFragment implements NotificationCenter.
                     DialogsActivity dialogsActivity = new DialogsActivity(arguments);
                     dialogsActivity.setDelegate(oldDelegate);
                     launchActivity.presentFragment(dialogsActivity, false, true);
-                } else if (id == pin || id == read || id == delete || id == clear || id == mute || id == archive) {
+                } else if (id == pin || id == read || id == delete || id == clear || id == mute || id == archive || id == profileInfo) {
                     perfromSelectedDialogsAction(id, true);
                 }
             }
@@ -955,6 +959,7 @@ public class DialogsActivity extends BaseFragment implements NotificationCenter.
         muteItem = otherItem.addSubItem(mute, R.drawable.msg_mute, LocaleController.getString("ChatsMute", R.string.ChatsMute));
         readItem = otherItem.addSubItem(read, R.drawable.msg_markread, LocaleController.getString("MarkAsRead", R.string.MarkAsRead));
         clearItem = otherItem.addSubItem(clear, R.drawable.msg_clear, LocaleController.getString("ClearHistory", R.string.ClearHistory));
+        profileInfoItem = otherItem.addSubItem(profileInfo, R.drawable.msg_message, LocaleController.getString("Info", R.string.Info));
 
         actionModeViews.add(pinItem);
         actionModeViews.add(archiveItem);
@@ -2382,6 +2387,11 @@ public class DialogsActivity extends BaseFragment implements NotificationCenter.
                         getNotificationsController().setDialogNotificationsSettings(selectedDialog, NotificationsController.SETTING_MUTE_FOREVER);
                     }
                 }
+            } else if (action == profileInfo) {
+                Bundle args = new Bundle();
+                final boolean isChat = (chat != null);
+                args.putInt(isChat ? "chat_id" : "user_id", isChat ? chat.id : user.id);
+                presentFragment(new ProfileActivity(args));
             }
         }
         if (action == pin) {
@@ -2532,6 +2542,7 @@ public class DialogsActivity extends BaseFragment implements NotificationCenter.
             pinItem.setIcon(R.drawable.msg_unpin);
             pinItem.setContentDescription(LocaleController.getString("UnpinFromTop", R.string.UnpinFromTop));
         }
+        profileInfoItem.setVisibility(count > 1 ? View.GONE : View.VISIBLE);
     }
 
     private boolean validateSlowModeDialog(long dialogId) {
