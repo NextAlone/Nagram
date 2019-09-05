ec0f935b9fe95e0618827828cf8de34722726f5d
23rd <23rd@vivaldi.net>
2019-09-05 19:31:52 +0300

    Added setting to hide or show personal information.

--- TMessagesProj/src/main/java/org/telegram/ui/ProfileActivity.java
+++ TMessagesProj/src/main/java/org/telegram/ui/ProfileActivity.java
@@ -106,6 +106,7 @@ import org.telegram.messenger.NotificationsController;
 import org.telegram.messenger.R;
 import org.telegram.messenger.SharedConfig;
 import org.telegram.messenger.UserConfig;
+import org.telegram.messenger.SharedConfig;
 import org.telegram.messenger.UserObject;
 import org.telegram.messenger.Utilities;
 import org.telegram.messenger.browser.Browser;
@@ -5096,10 +5097,17 @@ public class ProfileActivity extends BaseFragment implements NotificationCenter.
                     setAvatarRow = rowCount++;
                     setAvatarSectionRow = rowCount++;
                 }
-                numberSectionRow = rowCount++;
-                numberRow = rowCount++;
-                setUsernameRow = rowCount++;
-                bioRow = rowCount++;
+                if (SharedConfig.hideSensitiveData()) {
+                    numberSectionRow = -1;
+                    numberRow = -1;
+                    setUsernameRow = -1;
+                    bioRow = -1;
+                } else {
+                    numberSectionRow = rowCount++;
+                    numberRow = rowCount++;
+                    setUsernameRow = rowCount++;
+                    bioRow = rowCount++;
+                }
                 settingsSectionRow = rowCount++;
                 settingsSectionRow2 = rowCount++;
                 notificationRow = rowCount++;
@@ -5331,7 +5339,9 @@ public class ProfileActivity extends BaseFragment implements NotificationCenter.
         } else if (currentConnectionState == ConnectionsManager.ConnectionStateUpdating) {
             onlineTextOverride = LocaleController.getString("Updating", R.string.Updating);
         } else if (currentConnectionState == ConnectionsManager.ConnectionStateConnectingToProxy) {
-            onlineTextOverride = "...";
+            onlineTextOverride = SharedConfig.hideSensitiveData()
+                ? "..."
+                : LocaleController.getString("ConnectingToProxy", R.string.ConnectingToProxy);
         } else {
             onlineTextOverride = null;
         }
