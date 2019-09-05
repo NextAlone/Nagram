fa19aae807b81689ac64dfdd558baeed9b22a7bb
23rd <23rd@vivaldi.net>
2019-09-05 22:31:21 +0300

    Added ability to copy ID of users.

--- TMessagesProj/src/main/java/org/telegram/ui/ProfileActivity.java
+++ TMessagesProj/src/main/java/org/telegram/ui/ProfileActivity.java
@@ -241,6 +241,7 @@ public class ProfileActivity extends BaseFragment implements NotificationCenter.
     private int userInfoRow;
     private int channelInfoRow;
     private int usernameRow;
+    private int idRow;
     private int notificationsDividerRow;
     private int notificationsRow;
     private int infoSectionRow;
@@ -1287,6 +1288,17 @@ public class ProfileActivity extends BaseFragment implements NotificationCenter.
         listView.setAdapter(listAdapter);
         listView.setOnItemClickListener((view, position, x, y) -> {
             if (getParentActivity() == null) {
+                return;
+            }
+            if (position == idRow && user_id != 0) {
+                try {
+                    android.content.ClipboardManager clipboard = (android.content.ClipboardManager) ApplicationLoader.applicationContext.getSystemService(Context.CLIPBOARD_SERVICE);
+                    android.content.ClipData clip = android.content.ClipData.newPlainText("label", user_id + "");
+                    clipboard.setPrimaryClip(clip);
+                    Toast.makeText(getParentActivity(), LocaleController.getString("TextCopied", R.string.TextCopied), Toast.LENGTH_SHORT).show();
+                } catch (Exception e) {
+                    FileLog.e(e);
+                }
                 return;
             }
             if (position == photosRow || position == filesRow || position == linksRow || position == audioRow || position == voiceRow) {
@@ -3212,6 +3224,7 @@ public class ProfileActivity extends BaseFragment implements NotificationCenter.
         locationRow = -1;
         channelInfoRow = -1;
         usernameRow = -1;
+        idRow = -1;
         settingsTimerRow = -1;
         settingsKeyRow = -1;
         notificationsDividerRow = -1;
@@ -3270,6 +3283,9 @@ public class ProfileActivity extends BaseFragment implements NotificationCenter.
             }
             if (user != null && !TextUtils.isEmpty(user.username)) {
                 usernameRow = rowCount++;
+            }
+            if (user != null) {
+                idRow = rowCount++;
             }
             if (phoneRow != -1 || userInfoRow != -1 || usernameRow != -1) {
                 notificationsDividerRow = rowCount++;
@@ -3998,6 +4014,8 @@ public class ProfileActivity extends BaseFragment implements NotificationCenter.
                             text = LocaleController.getString("PhoneHidden", R.string.PhoneHidden);
                         }
                         detailCell.setTextAndValue(text, LocaleController.getString("PhoneMobile", R.string.PhoneMobile), false);
+                    } else if (position == idRow && user_id != 0) {
+                        detailCell.setTextAndValue(user_id + "", "ID", false);
                     } else if (position == usernameRow) {
                         String text;
                         if (user_id != 0) {
@@ -4243,7 +4261,7 @@ public class ProfileActivity extends BaseFragment implements NotificationCenter.
         public int getItemViewType(int i) {
             if (i == infoHeaderRow || i == sharedHeaderRow || i == membersHeaderRow) {
                 return 1;
-            } else if (i == phoneRow || i == usernameRow || i == locationRow) {
+            } else if (i == phoneRow || i == usernameRow || i == idRow || i == locationRow) {
                 return 2;
             } else if (i == userInfoRow || i == channelInfoRow) {
                 return 3;
