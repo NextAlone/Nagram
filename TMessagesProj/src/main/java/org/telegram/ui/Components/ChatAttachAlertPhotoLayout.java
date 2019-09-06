8777a3325e4c8333ace1d0585d1313eb5a4e6c11
23rd <23rd@vivaldi.net>
2019-09-06 18:52:26 +0300

    Added ability to use system camera for photos.
     - Slightly refactored settings.

--- TMessagesProj/src/main/java/org/telegram/ui/Components/ChatAttachAlertPhotoLayout.java
+++ TMessagesProj/src/main/java/org/telegram/ui/Components/ChatAttachAlertPhotoLayout.java
@@ -692,7 +692,8 @@ public class ChatAttachAlertPhotoLayout extends ChatAttachAlert.AttachAlertLayou
                     PhotoViewer.getInstance().setCaption(parentAlert.getCommentTextView().getText());
                 }
             } else {
-                if (SharedConfig.inappCamera) {
+                if (SharedConfig.inappCamera
+                    && !org.telegram.messenger.MessagesController.getGlobalMainSettings().getBoolean("systemCamera", false)) {
                     openCamera(true);
                 } else {
                     if (parentAlert.delegate != null) {
