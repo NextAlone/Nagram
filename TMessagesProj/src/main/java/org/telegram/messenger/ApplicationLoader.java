ab7bac9b37c5cb89ee9a034eb022dd52cfb1ade7
thermatk <thermatk@thermatk.com>
2020-01-12 14:21:13 +0100

    [TF][PUSH] default is true

--- TMessagesProj/src/main/java/org/telegram/messenger/ApplicationLoader.java
+++ TMessagesProj/src/main/java/org/telegram/messenger/ApplicationLoader.java
@@ -187,7 +187,7 @@ public class ApplicationLoader extends Application {
         if (preferences.contains("pushService")) {
             enabled = preferences.getBoolean("pushService", true);
         } else {
-            enabled = MessagesController.getMainSettings(UserConfig.selectedAccount).getBoolean("keepAliveService", false);
+            enabled = MessagesController.getMainSettings(UserConfig.selectedAccount).getBoolean("keepAliveService", true);
         }
         if (enabled) {
             try {
