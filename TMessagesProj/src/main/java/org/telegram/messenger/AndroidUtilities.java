515a040bb816943a4b7610e5f6fdf3d8fefe3ee6
NekoInverter <42698724+NekoInverter@users.noreply.github.com>
2020-02-26 17:10:51 +0800

    Fix

--- TMessagesProj/src/main/java/org/telegram/messenger/AndroidUtilities.java
+++ TMessagesProj/src/main/java/org/telegram/messenger/AndroidUtilities.java
@@ -546,30 +546,6 @@ public class AndroidUtilities {
 
     public static boolean isGoogleMapsInstalled(final BaseFragment fragment) {
         return true;
-<<<<<<< HEAD
-        /*try {
-            ApplicationLoader.applicationContext.getPackageManager().getApplicationInfo("com.google.android.apps.maps", 0);
-            return true;
-        } catch (PackageManager.NameNotFoundException e) {
-            if (fragment.getParentActivity() == null) {
-                return false;
-            }
-            AlertDialog.Builder builder = new AlertDialog.Builder(fragment.getParentActivity());
-            builder.setMessage(LocaleController.getString("InstallGoogleMaps", R.string.InstallGoogleMaps));
-            builder.setPositiveButton(LocaleController.getString("OK", R.string.OK), (dialogInterface, i) -> {
-                try {
-                    Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=com.google.android.apps.maps"));
-                    fragment.getParentActivity().startActivityForResult(intent, 500);
-                } catch (Exception e1) {
-                    FileLog.e(e1);
-                }
-            });
-            builder.setNegativeButton(LocaleController.getString("Cancel", R.string.Cancel), null);
-            fragment.showDialog(builder.create());
-            return false;
-        }*/
-=======
->>>>>>> e45f1329... [TF][MAPS] replace Google Maps with OSMDroid
     }
 
     public static int[] toIntArray(List<Integer> integers) {
