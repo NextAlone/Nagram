10679847ca22c20a70300ef019a325fc5c88b844
23rd <23rd@vivaldi.net>
2020-04-20 12:38:06 +0300

    Added new button to settings to check updates.

--- TMessagesProj/src/main/java/org/telegram/ui/ProfileActivity.java
+++ TMessagesProj/src/main/java/org/telegram/ui/ProfileActivity.java
@@ -397,6 +397,7 @@ public class ProfileActivity extends BaseFragment implements NotificationCenter.
     private int forkHeaderRow;
     private int forkSectionCell;
     private int forkRow;
+    private int forkCheckUpdateRow;
 
     private final Property<ProfileActivity, Float> HEADER_SHADOW = new AnimationProperties.FloatProperty<ProfileActivity>("headerShadow") {
         @Override
@@ -2380,6 +2381,8 @@ public class ProfileActivity extends BaseFragment implements NotificationCenter.
                 presentFragment(new SessionsActivity(0));
             } else if (position == forkRow) {
                 presentFragment(new ForkSettingsActivity());
+            } else if (position == forkCheckUpdateRow) {
+                ((LaunchActivity) getParentActivity()).checkAppUpdate(true);
             } else if (position == questionRow) {
                 showDialog(AlertsCreator.createSupportAlert(ProfileActivity.this));
             } else if (position == faqRow) {
@@ -4807,6 +4810,7 @@ public class ProfileActivity extends BaseFragment implements NotificationCenter.
                 devicesSectionRow = rowCount++;
                 forkHeaderRow = rowCount++;
                 forkRow = rowCount++;
+                forkCheckUpdateRow = rowCount++;
                 forkSectionCell = rowCount++;
                 helpHeaderRow = rowCount++;
                 questionRow = rowCount++;
@@ -6275,6 +6279,8 @@ public class ProfileActivity extends BaseFragment implements NotificationCenter.
                         textCell.setTextAndIcon(LocaleController.getString("ChatSettings", R.string.ChatSettings), R.drawable.menu_chats, true);
                     } else if (position == forkRow) {
                         textCell.setTextAndIcon(LocaleController.getString("ForkSettingsTitle", R.string.ForkSettingsTitle), R.drawable.menu_fork, true);
+                    } else if (position == forkCheckUpdateRow) {
+                        textCell.setText(LocaleController.getString("ForkCheckUpdate", R.string.ForkCheckUpdate), true);
                     } else if (position == filtersRow) {
                         textCell.setTextAndIcon(LocaleController.getString("Filters", R.string.Filters), R.drawable.menu_folders, true);
                     } else if (position == questionRow) {
@@ -6424,7 +6430,7 @@ public class ProfileActivity extends BaseFragment implements NotificationCenter.
                         position == versionRow || position == dataRow || position == chatRow ||
                         position == questionRow || position == devicesRow || position == filtersRow ||
                         position == faqRow || position == policyRow || position == sendLogsRow ||
-                        position == forkRow ||
+                        position == forkRow || position == forkCheckUpdateRow ||
                         position == clearLogsRow || position == switchBackendRow || position == setAvatarRow;
             }
             int type = holder.getItemViewType();
@@ -6442,7 +6448,7 @@ public class ProfileActivity extends BaseFragment implements NotificationCenter.
                 return 7;
             } else if (position == forkHeaderRow) {
                 return 1;
-            } else if (position == forkRow) {
+            } else if (position == forkRow || position == forkCheckUpdateRow) {
                 return 4;
             }
             if (position == infoHeaderRow || position == membersHeaderRow || position == settingsSectionRow2 ||
