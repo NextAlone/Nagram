822a7ba1e36a0c77216fdfb24c5b1820703f5a85
Federico Armellini <armellini.federico@gmail.com>
2020-06-10 09:46:54 +0200

    check if google play service installed before show google contact popup

--- TMessagesProj/src/main/java/org/telegram/ui/LaunchActivity.java
+++ TMessagesProj/src/main/java/org/telegram/ui/LaunchActivity.java
@@ -4800,6 +4800,10 @@ public class LaunchActivity extends BasePermissionsActivity implements ActionBar
         checkLayout();
     }
 
+    private boolean checkPlayServices() {
+        return false;
+    }
+
     @Override
     @SuppressWarnings("unchecked")
     public void didReceivedNotification(int id, final int account, Object... args) {
@@ -4937,6 +4941,11 @@ public class LaunchActivity extends BasePermissionsActivity implements ActionBar
             if (actionBarLayout == null || actionBarLayout.fragmentsStack.isEmpty()) {
                 return;
             }
+
+            if (!checkPlayServices()) {
+                return;
+            }
+
             final int type = (Integer) args[0];
             final HashMap<String, ContactsController.Contact> contactHashMap = (HashMap<String, ContactsController.Contact>) args[1];
             final boolean first = (Boolean) args[2];
