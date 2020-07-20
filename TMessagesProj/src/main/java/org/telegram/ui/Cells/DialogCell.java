53a6d502a6b469e3baf2775df13fde8b153964fb
23rd <23rd@vivaldi.net>
2020-07-21 00:10:49 +0300

    Added option to disable thumbs in dialog list.

--- TMessagesProj/src/main/java/org/telegram/ui/Cells/DialogCell.java
+++ TMessagesProj/src/main/java/org/telegram/ui/Cells/DialogCell.java
@@ -964,6 +964,9 @@ public class DialogCell extends BaseCell {
                             }
                             currentMessagePaint = Theme.dialogs_messagePrintingPaint[paintIndex];
                         } else {
+                            final boolean disableThumbs = MessagesController
+                                .getGlobalMainSettings()
+                                .getBoolean("disableThumbsInDialogList", false);
                             boolean needEmoji = true;
                             if (TextUtils.isEmpty(restrictionReason) && currentDialogFolderId == 0 && encryptedChat == null && !message.needDrawBluredPreview() && (message.isPhoto() || message.isNewGif() || message.isVideo())) {
                                 String type = message.isWebpage() ? message.messageOwner.media.webpage.type : null;
@@ -988,7 +991,7 @@ public class DialogCell extends BaseCell {
                                         } else {
                                             thumbImage.setImage(null, null, ImageLocation.getForObject(smallThumb, message.photoThumbsObject), "20_20", (Drawable) null, message, 0);
                                         }
-                                        needEmoji = false;
+                                        needEmoji = disableThumbs;
                                     }
                                 }
                             }
@@ -1117,9 +1120,11 @@ public class DialogCell extends BaseCell {
                                         messageString = new SpannableStringBuilder(messageString);
                                     }
                                     checkMessage = false;
+                                    if (!disableThumbs) {
                                     SpannableStringBuilder builder = (SpannableStringBuilder) messageString;
                                     builder.insert(thumbInsertIndex, " ");
                                     builder.setSpan(new FixedWidthSpan(AndroidUtilities.dp(thumbSize + 6)), thumbInsertIndex, thumbInsertIndex + 1, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+                                    }
                                 }
                             } else {
                                 if (!TextUtils.isEmpty(restrictionReason)) {
@@ -1207,6 +1212,7 @@ public class DialogCell extends BaseCell {
                                         messageString = new SpannableStringBuilder(messageString);
                                     }
                                     checkMessage = false;
+                                    if (!disableThumbs) {
                                     SpannableStringBuilder builder = (SpannableStringBuilder) messageString;
                                     builder.insert(0, " ");
                                     builder.setSpan(new FixedWidthSpan(AndroidUtilities.dp(thumbSize + 6)), 0, 1, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
@@ -1216,6 +1222,7 @@ public class DialogCell extends BaseCell {
                                         if (s != null) {
                                             messageString = s;
                                         }
+                                    }
                                     }
                                 }
                             }
@@ -2792,7 +2799,10 @@ public class DialogCell extends BaseCell {
             avatarImage.draw(canvas);
         }
 
-        if (hasMessageThumb) {
+        if (hasMessageThumb
+            && !MessagesController
+                .getGlobalMainSettings()
+                .getBoolean("disableThumbsInDialogList", false)) {
             thumbImage.draw(canvas);
             if (drawPlay) {
                 int x = (int) (thumbImage.getCenterX() - Theme.dialogs_playDrawable.getIntrinsicWidth() / 2);
