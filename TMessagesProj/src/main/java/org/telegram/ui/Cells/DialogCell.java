c841301d1da6a31b3d4ec6c99d8b58bf7e98c2bb
23rd <23rd@vivaldi.net>
2020-07-21 00:10:49 +0300

    Added option to disable thumbs in dialog list.

--- TMessagesProj/src/main/java/org/telegram/ui/Cells/DialogCell.java
+++ TMessagesProj/src/main/java/org/telegram/ui/Cells/DialogCell.java
@@ -845,6 +845,9 @@ public class DialogCell extends BaseCell {
                             }
                             currentMessagePaint = Theme.dialogs_messagePrintingPaint[paintIndex];
                         } else {
+                            final boolean disableThumbs = MessagesController
+                                .getGlobalMainSettings()
+                                .getBoolean("disableThumbsInDialogList", false);
                             boolean needEmoji = true;
                             if (currentDialogFolderId == 0 && encryptedChat == null && !message.needDrawBluredPreview() && (message.isPhoto() || message.isNewGif() || message.isVideo())) {
                                 String type = message.isWebpage() ? message.messageOwner.media.webpage.type : null;
@@ -963,9 +966,11 @@ public class DialogCell extends BaseCell {
                                         messageString = new SpannableStringBuilder(messageString);
                                     }
                                     checkMessage = false;
+                                    if (!disableThumbs) {
                                     SpannableStringBuilder builder = (SpannableStringBuilder) messageString;
                                     builder.insert(thumbInsertIndex, " ");
                                     builder.setSpan(new FixedWidthSpan(AndroidUtilities.dp(thumbSize + 6)), thumbInsertIndex, thumbInsertIndex + 1, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+                                    }
                                 }
                             } else {
                                 if (message.messageOwner.media instanceof TLRPC.TL_messageMediaPhoto && message.messageOwner.media.photo instanceof TLRPC.TL_photoEmpty && message.messageOwner.media.ttl_seconds != 0) {
@@ -1012,10 +1017,12 @@ public class DialogCell extends BaseCell {
                                         messageString = new SpannableStringBuilder(messageString);
                                     }
                                     checkMessage = false;
+                                    if (!disableThumbs) {
                                     SpannableStringBuilder builder = (SpannableStringBuilder) messageString;
                                     builder.insert(0, " ");
                                     builder.setSpan(new FixedWidthSpan(AndroidUtilities.dp(thumbSize + 6)), 0, 1, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
                                     Emoji.replaceEmoji(builder, Theme.dialogs_messagePaint[paintIndex].getFontMetricsInt(), AndroidUtilities.dp(17), false);
+                                    }
                                 }
                             }
                         }
@@ -2196,7 +2203,10 @@ public class DialogCell extends BaseCell {
             avatarImage.draw(canvas);
         }
 
-        if (hasMessageThumb) {
+        if (hasMessageThumb
+            && !MessagesController
+                .getGlobalMainSettings()
+                .getBoolean("disableThumbsInDialogList", false)) {
             thumbImage.draw(canvas);
             if (drawPlay) {
                 int x = (int) (thumbImage.getCenterX() - Theme.dialogs_playDrawable.getIntrinsicWidth() / 2);
