97ef5f33e23632d00e2869bd6a094ac09d06a0b3
世界 <i@nekox.me>
2020-08-01 10:33:54 +0800

    Refinds

---
+++ bin/upload_alaha.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+outPath="TMessagesProj/build/outputs/apk/full/release"
+
+#./gradlew TMessagesProj:assembleFullRelease || exit 1
+
+rm -f $outPath/*universal*
+
+for apk in $outPath/*.apk; do
+
+    echo ">> Uploading $apk"
+
+    curl https://api.telegram.org/bot${TELEGRAM_TOKEN}/sendDocument \
+        -X POST \
+        -F chat_id="$TELEGRAM_CHANNEL" \
+        -F document="@$apk" \
+        --silent --show-error --fail > /dev/null &
+
+done
+
+trap 'kill $(jobs -p)' SIGINT
+
+for job in `jobs -p`; do
+  wait $job
+done
