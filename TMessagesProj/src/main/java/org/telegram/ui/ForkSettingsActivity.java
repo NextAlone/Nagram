yesnomaybeyes/TelegramAndroid

Added setting to set default camera for Video Messages.
 - Fixes #1.

TMessagesProj/src/main/java/org/telegram/ui/ForkSettingsActivity.java
@@ -54,6 +54,7 @@ public class ForkSettingsActivity extends BaseFragment {
     private int photoHasStickerRow;
     private int unmutedOnTopRow;
     private int shareAnonymRow;
+    private int rearVideoMessages;
 
     private int syncPinsRow;
     private int pinOrderRow;
@@ -72,6 +73,7 @@ public class ForkSettingsActivity extends BaseFragment {
         photoHasStickerRow = rowCount++;
         unmutedOnTopRow = rowCount++;
         shareAnonymRow = rowCount++;
+        rearVideoMessages = rowCount++;
 
         emptyRow = rowCount++;
         sectionRow2 = rowCount++;
@@ -156,6 +158,8 @@ public class ForkSettingsActivity extends BaseFragment {
                 MessagesController.getInstance(currentAccount).sortDialogs(null);
             } else if (position == shareAnonymRow) {
                 toggleGlobalMainSetting("shareAlertAsAnonym", view, true);
+            } else if (position == rearVideoMessages) {
+                toggleGlobalMainSetting("rearVideoMessages", view, false);
             } else if (position == pinOrderRow) {
                 presentFragment(new PinsOrderActivity());
             }
@@ -221,6 +225,9 @@ public class ForkSettingsActivity extends BaseFragment {
                     } else if (position == shareAnonymRow) {
                         String t = LocaleController.getString("ShareAlertAnonym", R.string.ShareAlertAnonym);
                         textCell.setTextAndCheck(t, preferences.getBoolean("shareAlertAsAnonym", true), true);
+                    } else if (position == rearVideoMessages) {
+                        String t = LocaleController.getString("RearVideoMessages", R.string.RearVideoMessages);
+                        textCell.setTextAndCheck(t, preferences.getBoolean("rearVideoMessages", false), false);
                     }
                     break;
                 }
@@ -245,7 +252,8 @@ public class ForkSettingsActivity extends BaseFragment {
                         || position == syncPinsRow
                         || position == unmutedOnTopRow
                         || position == shareAnonymRow
-                        || position == pinOrderRow;
+                        || position == pinOrderRow
+                        || position == rearVideoMessages;
             return fork;
         }
 
@@ -292,6 +300,7 @@ public class ForkSettingsActivity extends BaseFragment {
                 || position == syncPinsRow
                 || position == unmutedOnTopRow
                 || position == shareAnonymRow
+                || position == rearVideoMessages
                 || position == photoHasStickerRow) {
                 return 3;
             } else if (position == sectionRow1

NextAlone/Nagram

TMessagesProj/src/main/java/org/telegram/ui/ForkSettingsActivity.java
@@ -45,6 +45,7 @@ public class ForkSettingsActivity extends BaseFragment {
     private int inappCameraRow;
     private int photoHasStickerRow;
     private int unmutedOnTopRow;
+    private int rearVideoMessages;
 
     private int emptyRow;
 
@@ -59,6 +60,7 @@ public class ForkSettingsActivity extends BaseFragment {
         inappCameraRow = rowCount++;
         photoHasStickerRow = rowCount++;
         unmutedOnTopRow = rowCount++;
+        rearVideoMessages = rowCount++;
 
         return true;
     }
@@ -122,6 +124,8 @@ public class ForkSettingsActivity extends BaseFragment {
             } else if (position == unmutedOnTopRow) {
                 toggleGlobalMainSetting("unmutedOnTop", view, false);
                 MessagesController.getInstance(currentAccount).sortDialogs(null);
+            } else if (position == rearVideoMessages) {
+                toggleGlobalMainSetting("rearVideoMessages", view, false);
             }
         });
 
@@ -175,6 +179,9 @@ public class ForkSettingsActivity extends BaseFragment {
                         String t = LocaleController.getString("UnmutedOnTop", R.string.UnmutedOnTop);
                         String info = LocaleController.getString("UnmutedOnTopInfo", R.string.UnmutedOnTopInfo);
                         textCell.setTextAndValueAndCheck(t, info, preferences.getBoolean("unmutedOnTop", false), true, false);
+                    } else if (position == rearVideoMessages) {
+                        String t = LocaleController.getString("RearVideoMessages", R.string.RearVideoMessages);
+                        textCell.setTextAndCheck(t, preferences.getBoolean("rearVideoMessages", false), false);
                     }
                     break;
                 }
@@ -193,6 +200,8 @@ public class ForkSettingsActivity extends BaseFragment {
             int position = holder.getAdapterPosition();
             boolean fork = position == squareAvatarsRow
                         || position == inappCameraRow
+                        || position == unmutedOnTopRow
+                        || position == rearVideoMessages
                         || position == photoHasStickerRow;
             return fork;
         }
@@ -238,6 +247,7 @@ public class ForkSettingsActivity extends BaseFragment {
             } else if (position == squareAvatarsRow
                 || position == inappCameraRow
                 || position == unmutedOnTopRow
+                || position == rearVideoMessages
                 || position == photoHasStickerRow) {
                 return 3;
             } else if (position == sectionRow1) {