f0e45007d1aa444a476eb8fa1734141aecb6aae9
世界 <i@nekox.me>
2021-02-28 15:08:33 +0800

    Ignore x86 for f-droid build

--- ss-rust/build.gradle.kts
+++ ss-rust/build.gradle.kts
@@ -1,9 +1,30 @@
+import cn.hutool.core.codec.Base64
 import com.android.build.gradle.internal.tasks.factory.dependsOn
+import org.apache.tools.ant.filters.StringInputStream
+import java.util.*
+import java.io.*
 
 plugins {
     id("com.android.library")
     id("org.mozilla.rust-android-gradle.rust-android")
 }
+
+var ignoreX86 = false
+
+lateinit var properties: Properties
+val base64 = System.getenv("LOCAL_PROPERTIES")
+if (!base64.isNullOrBlank()) {
+    properties = Properties()
+    properties.load(ByteArrayInputStream(Base64.decode(base64)))
+} else if (project.rootProject.file("local.properties").exists()) {
+    properties = Properties()
+    properties.load(StringInputStream(project.rootProject.file("local.properties").readText()))
+}
+
+if (::properties.isInitialized) {
+    ignoreX86 = properties.getProperty("IGNORE_X86") == "true"
+}
+
 
 android {
 
@@ -16,12 +37,22 @@ android {
     }
     buildToolsVersion = "30.0.3"
 
+    if (ignoreX86) {
+        splits.abi {
+            exclude("x86", "x86_64")
+        }
+    }
+
 }
 
 cargo {
     module = "src/main/rust/shadowsocks-rust"
     libname = "ss-local"
-    targets = listOf("arm", "arm64", "x86", "x86_64")
+    targets = if (ignoreX86) {
+        listOf("arm", "arm64")
+    } else {
+        listOf("arm", "arm64", "x86", "x86_64")
+    }
     profile = findProperty("CARGO_PROFILE")?.toString() ?: "release"
     extraCargoBuildArguments = listOf("--bin", "sslocal")
     featureSpec.noDefaultBut(arrayOf(
