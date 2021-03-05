2b7a6229fc1c947392c1be66635c435158da2ae5
世界 <i@nekox.me>
2021-03-05 12:10:37 +0800

    Refine v2ray-core

--- bin/libs/v2ray/build.sh
+++ bin/libs/v2ray/build.sh
@@ -4,7 +4,7 @@ source "bin/init/env.sh"
 export GO111MOUDLE=on
 export PATH="$PATH:$(go env GOPATH)/bin"
 
-cd "$PROJECT/build/v2ray"
+cd "$PROJECT/v2ray"
 gomobile init
 gomobile bind -v -ldflags='-s -w' . || exit 1
 
--- bin/libs/v2ray/init.sh
+++ bin/libs/v2ray/init.sh
@@ -1,18 +1,11 @@
 #!/usr/bin/env bash
 
-# v4.34.0
-V2RAY_CORE_VERSION="a6efb4d60b86789a68ed8ac8d52cfcec2d80229a"
-
 source "bin/init/env.sh"
 export GO111MOUDLE=on
 export PATH="$PATH:$(go env GOPATH)/bin"
 
-mkdir -p "$PROJECT/build"
-cd "$PROJECT/build"
-[ -d "v2ray" ] || git clone https://github.com/2dust/AndroidLibV2rayLite v2ray || exit 1
+cd $PROJECT
+[ -f v2ray/go.mod ] || git submodule update --init v2ray
 cd v2ray
 git reset --hard && git clean -fdx
-sed -i -e "s|go 1.14|go 1.16|g" go.mod
-sed -i -e "s|core master|core $V2RAY_CORE_VERSION|g" go.mod
-go mod download -x || exit 1
-go get -v golang.org/x/mobile/cmd/...
+go mod download -x && go get -v golang.org/x/mobile/cmd/... || exit 1
