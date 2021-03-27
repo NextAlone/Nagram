68a7e5a51bd095730c99c8d4812a1a3c03275661
世界 <sekai@neko.services>
2021-03-27 09:45:50 +0800

    Fix clear history

--- TMessagesProj/src/main/java/org/telegram/ui/Components/ClearHistoryAlert.java
+++ TMessagesProj/src/main/java/org/telegram/ui/Components/ClearHistoryAlert.java
@@ -231,7 +231,7 @@ public class ClearHistoryAlert extends BottomSheet {
             revokeTimeLimit = MessagesController.getInstance(currentAccount).revokeTimeLimit;
         }
         boolean canDeleteInbox = user != null && canRevokeInbox && revokeTimeLimit == 0x7fffffff;
-        final boolean[] deleteForAll = new boolean[]{false};
+        final boolean[] deleteForAll = new boolean[]{true};
         boolean deleteChatForAll = false;
 
         if (!autoDeleteOnly) {
@@ -266,7 +266,7 @@ public class ClearHistoryAlert extends BottomSheet {
             if (canDeleteInbox && !UserObject.isDeleted(user)) {
                 cell = new CheckBoxCell(context, 1);
                 cell.setBackgroundDrawable(Theme.getSelectorDrawable(false));
-                cell.setText(LocaleController.formatString("ClearHistoryOptionAlso", R.string.ClearHistoryOptionAlso, UserObject.getFirstName(user)), "", false, false);
+                cell.setText(LocaleController.formatString("ClearHistoryOptionAlso", R.string.ClearHistoryOptionAlso, UserObject.getFirstName(user)), "", deleteForAll[0], false);
                 cell.setPadding(LocaleController.isRTL ? AndroidUtilities.dp(16) : AndroidUtilities.dp(5), 0, LocaleController.isRTL ? AndroidUtilities.dp(5) : AndroidUtilities.dp(16), 0);
                 linearLayout.addView(cell, LayoutHelper.createLinear(LayoutHelper.MATCH_PARENT, 48, Gravity.TOP | Gravity.LEFT, 0, 0, 0, 0));
                 cell.setOnClickListener(v -> {
