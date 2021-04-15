26a8ffe73790b9a3ceac5e487dd1ca2b0643d949
freetux <thefreetux@gmail.com>
2021-04-15 09:49:48 +0430

    Update AlertUtil.kt

--- TMessagesProj/src/main/java/tw/nekomimi/nekogram/utils/AlertUtil.kt
+++ TMessagesProj/src/main/java/tw/nekomimi/nekogram/utils/AlertUtil.kt
@@ -98,7 +98,7 @@ object AlertUtil {
 
         val builder = AlertDialog.Builder(ctx)
 
-        builder.setTitle(title ?: LocaleController.getString("Freegram", R.string.Freegram))
+        builder.setTitle(title ?: LocaleController.getString("Freegram", R.string.nekox))
         builder.setMessage(text)
 
         builder.setPositiveButton(LocaleController.getString("OK", R.string.OK)) { _, _ ->
