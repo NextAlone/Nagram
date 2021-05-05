d6bd7e6cf2d05fac91fc7f2a3a6d8f5cdec6e56a
herotux <thefreetux@gmail.com>
2021-05-06 00:37:46 +0430

    minor fix

--- TMessagesProj/src/main/java/org/telegram/ui/Cells/ArchiveHintInnerCell.java
+++ TMessagesProj/src/main/java/org/telegram/ui/Cells/ArchiveHintInnerCell.java
@@ -7,7 +7,7 @@ import android.util.TypedValue;
 import android.view.Gravity;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
-
+import android.widget.TextView;
 
 import org.telegram.messenger.AndroidUtilities;
 import org.telegram.messenger.LocaleController;
