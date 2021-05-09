e8ff4351be6727e2cfea210fc355c8c8c4ce40e9
luvletter2333 <luvletter2333@gmail.com>
2021-05-09 23:52:28 +0800

    fix jump to beginning button display

--- TMessagesProj/src/main/java/org/telegram/ui/ChatActivity.java
+++ TMessagesProj/src/main/java/org/telegram/ui/ChatActivity.java
@@ -7213,8 +7213,7 @@ public class ChatActivity extends BaseFragment implements NotificationCenter.Not
         searchGoToBeginningButton.setImageResource(R.drawable.baseline_arrow_upward_24);
         searchGoToBeginningButton.setColorFilter(new PorterDuffColorFilter(Theme.getColor(Theme.key_chat_searchPanelIcons), PorterDuff.Mode.SRC_IN));
         searchGoToBeginningButton.setBackgroundDrawable(Theme.createSelectorDrawable(Theme.getColor(Theme.key_actionBarActionModeDefaultSelector), 1));
-        boolean showSearchUserButton = currentChat != null && (!ChatObject.isChannel(currentChat) || currentChat.megagroup);
-        searchContainer.addView(searchGoToBeginningButton, LayoutHelper.createFrame(48, 48, Gravity.LEFT | Gravity.TOP, showSearchUserButton ? 48 * 2 : 48, 0, 0, 0));
+        searchContainer.addView(searchGoToBeginningButton, LayoutHelper.createFrame(48, 48, Gravity.RIGHT | Gravity.TOP, 0, 0, 48 * 2, 0));
         searchGoToBeginningButton.setOnClickListener(view -> {
             scrollToMessageId(1, 0, false, 0, true, 0);
         });
