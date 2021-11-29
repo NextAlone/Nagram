7e19c1e23cf4f7c22db7a339793889680e2b1067
AlexandrKozlovskiy <k.sasha1994@gmail.com>
2021-11-29 22:12:50 +0800

    feat: add information about mutual contacts

--- TMessagesProj/src/main/java/org/telegram/ui/Cells/ChatMessageCell.java
+++ TMessagesProj/src/main/java/org/telegram/ui/Cells/ChatMessageCell.java
@@ -14526,6 +14526,10 @@ public class ChatMessageCell extends BaseCell implements SeekBar.SeekBarDelegate
         }
     }
 
+    public boolean isSeekbarCell() {
+        return currentMessageObject.isVoice() || currentMessageObject.isRoundVideo() || currentMessageObject.isMusic();
+    }
+
     @Override
     public boolean onHoverEvent(MotionEvent event) {
         int x = (int) event.getX();
