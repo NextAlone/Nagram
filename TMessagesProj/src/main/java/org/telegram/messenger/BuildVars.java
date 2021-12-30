f8eb8ab357a0f557062a35f51e05e04c2919882c
Nikita S <nikita@saraeff.net>
2021-12-30 20:53:48 +0100

    updated api and hash
    Public Android Beta
    APP_CONFIG_API_ID = 4
    APP_CONFIG_API_HASH = "014b35b6184100b085b0d0572f9b5103"
    
    Public Static Final
    APP_CONFIG_API_ID = 5
    APP_CONFIG_API_HASH = "1c5c96d5edd401b1ed40db3fb5633e2d"
    
    Distributed Android
    APP_CONFIG_API_ID = 6
    APP_CONFIG_API_HASH = "eb06d4abfb49dc3eeb1aeb98ae0f581e"
    
    Public iOS Beta
    APP_CONFIG_API_ID = 8
    APP_CONFIG_API_HASH = "7245de8e747a0d6fbe11f7cc14fcc0bb"

--- TMessagesProj/src/main/java/org/telegram/messenger/BuildVars.java
+++ TMessagesProj/src/main/java/org/telegram/messenger/BuildVars.java
@@ -22,8 +22,8 @@ public class BuildVars {
     public static boolean NO_SCOPED_STORAGE = Build.VERSION.SDK_INT <= 29;
     public static int BUILD_VERSION = 2526;
     public static String BUILD_VERSION_STRING = "8.4.2";
-    public static int APP_ID = 4;
-    public static String APP_HASH = "014b35b6184100b085b0d0572f9b5103";
+    public static int APP_ID = 6;
+    public static String APP_HASH = "eb06d4abfb49dc3eeb1aeb98ae0f581e";
 
     public static String SMS_HASH = isStandaloneApp() ? "w0lkcmTZkKh" : (DEBUG_VERSION ? "O2P2z+/jBpJ" : "oLeq9AcOZkT");
     public static String PLAYSTORE_APP_URL = "https://play.google.com/store/apps/details?id=org.telegram.messenger";
