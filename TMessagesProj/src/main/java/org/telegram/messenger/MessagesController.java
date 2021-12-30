03307a9d90b86bcb5864880592110c4ee3427ebb
Nikita S <nikita@saraeff.net>
2021-12-30 20:46:21 +0100

    allow load banned chats on start * ChatObject.java   only `null` or `left==true` will return success for `isNotInChat`. `null` - thats simple, but `left` is when you left a group by yourself and deleted it. w/o `left` it will have ugly bug when you have chat UI active can type and send messages but messages are all have errors due you're not in chat. * MessagesController.java   here is simple, load all the messages.

--- TMessagesProj/src/main/java/org/telegram/messenger/MessagesController.java
+++ TMessagesProj/src/main/java/org/telegram/messenger/MessagesController.java
@@ -7923,13 +7923,14 @@ public class MessagesController extends BaseController implements NotificationCe
                 AndroidUtilities.runOnUIThread(() -> checkChatInviter(chat.id, true));
             }
 
-            TLRPC.Message lastMessageFinal = lastMessage;
+//            TLRPC.Message lastMessageFinal = lastMessage;
             AndroidUtilities.runOnUIThread(() -> {
-                if (lastMessageFinal != null) {
-                    dialogsLoadedTillDate = Math.min(dialogsLoadedTillDate, lastMessageFinal.date);
-                } else {
-                    dialogsLoadedTillDate = Integer.MIN_VALUE;
-                }
+//                if (lastMessageFinal != null) {
+//                    dialogsLoadedTillDate = Math.min(dialogsLoadedTillDate, lastMessageFinal.date);
+//                } else {
+//                    dialogsLoadedTillDate = Integer.MIN_VALUE;
+//                }
+                dialogsLoadedTillDate = Integer.MIN_VALUE;
                 if (loadType != DIALOGS_LOAD_TYPE_CACHE) {
                     applyDialogsNotificationsSettings(dialogsRes.dialogs);
                     getMediaDataController().loadDraftsIfNeed();
