9b87154dce324cb4eeb0cd3c02e32f7815c67359
Nikita S <nikita@saraeff.net>
2021-12-30 20:44:40 +0100

    no more edit_hide Telegram server can't force your vanilla client to hide "edited" marks anymore

--- TMessagesProj/src/main/java/org/telegram/tgnet/TLRPC.java
+++ TMessagesProj/src/main/java/org/telegram/tgnet/TLRPC.java
@@ -54122,7 +54122,7 @@ public class TLRPC {
             post = (flags & 16384) != 0;
             from_scheduled = (flags & 262144) != 0;
             legacy = (flags & 524288) != 0;
-            edit_hide = (flags & 2097152) != 0;
+            edit_hide = false;
             pinned = (flags & 16777216) != 0;
             noforwards = (flags & 67108864) != 0;
             id = stream.readInt32(exception);
@@ -54299,7 +54299,7 @@ public class TLRPC {
             post = (flags & 16384) != 0;
             from_scheduled = (flags & 262144) != 0;
             legacy = (flags & 524288) != 0;
-            edit_hide = (flags & 2097152) != 0;
+            edit_hide = false;
             pinned = (flags & 16777216) != 0;
             id = stream.readInt32(exception);
             if ((flags & 256) != 0) {
@@ -54474,7 +54474,7 @@ public class TLRPC {
             post = (flags & 16384) != 0;
             from_scheduled = (flags & 262144) != 0;
             legacy = (flags & 524288) != 0;
-            edit_hide = (flags & 2097152) != 0;
+            edit_hide = false;
             pinned = (flags & 16777216) != 0;
             id = stream.readInt32(exception);
             if ((flags & 256) != 0) {
@@ -54643,7 +54643,7 @@ public class TLRPC {
             post = (flags & 16384) != 0;
             from_scheduled = (flags & 262144) != 0;
             legacy = (flags & 524288) != 0;
-            edit_hide = (flags & 2097152) != 0;
+            edit_hide = false;
             id = stream.readInt32(exception);
             if ((flags & 256) != 0) {
                 from_id = new TLRPC.TL_peerUser();
@@ -54806,7 +54806,7 @@ public class TLRPC {
             post = (flags & 16384) != 0;
             from_scheduled = (flags & 262144) != 0;
             legacy = (flags & 524288) != 0;
-            edit_hide = (flags & 2097152) != 0;
+            edit_hide = false;
             id = stream.readInt32(exception);
             if ((flags & 256) != 0) {
                 from_id = new TLRPC.TL_peerUser();
@@ -54963,7 +54963,7 @@ public class TLRPC {
             post = (flags & 16384) != 0;
             from_scheduled = (flags & 262144) != 0;
             legacy = (flags & 524288) != 0;
-            edit_hide = (flags & 2097152) != 0;
+            edit_hide = false;
             id = stream.readInt32(exception);
             if ((flags & 256) != 0) {
                 from_id = new TLRPC.TL_peerUser();
@@ -55126,7 +55126,7 @@ public class TLRPC {
             post = (flags & 16384) != 0;
             from_scheduled = (flags & 262144) != 0;
             legacy = (flags & 524288) != 0;
-            edit_hide = (flags & 2097152) != 0;
+            edit_hide = false;
             id = stream.readInt32(exception);
             if ((flags & 256) != 0) {
                 from_id = new TLRPC.TL_peerUser();
