52de554d78dff7b03808da0b307cfca80b2aaaf5
qwq233 <qwq233@qwq2333.top>
2022-01-02 17:31:36 +0800

    init with modified name
    Signed-off-by: qwq233 <qwq233@qwq2333.top>

--- TMessagesProj/src/main/java/org/telegram/messenger/BuildVars.java
+++ TMessagesProj/src/main/java/org/telegram/messenger/BuildVars.java
@@ -15,15 +15,16 @@ import android.os.Build;
 public class BuildVars {
 
     public static boolean DEBUG_VERSION = false;
-    public static boolean LOGS_ENABLED = false;
     public static boolean DEBUG_PRIVATE_VERSION = false;
-    public static boolean USE_CLOUD_STRINGS = true;
     public static boolean CHECK_UPDATES = true;
-    public static boolean NO_SCOPED_STORAGE = Build.VERSION.SDK_INT <= 29;
     public static int BUILD_VERSION = 2526;
     public static String BUILD_VERSION_STRING = "8.4.2";
-    public static int APP_ID = 4;
-    public static String APP_HASH = "014b35b6184100b085b0d0572f9b5103";
+    public static boolean LOGS_ENABLED = true;
+    public static boolean USE_CLOUD_STRINGS = true;
+    public static boolean NO_SCOPED_STORAGE = Build.VERSION.SDK_INT <= 29;
+    public static int APP_ID = 19797609;
+    public static String APP_HASH = "e8f1567dbbf38944a1391c4d23c34b60";
+    public static String APPCENTER_HASH = "e07b49da-11a5-46db-a780-f5cd7b9a1a5a";
 
     public static String SMS_HASH = isStandaloneApp() ? "w0lkcmTZkKh" : (DEBUG_VERSION ? "O2P2z+/jBpJ" : "oLeq9AcOZkT");
     public static String PLAYSTORE_APP_URL = "https://play.google.com/store/apps/details?id=org.telegram.messenger";
@@ -46,7 +47,7 @@ public class BuildVars {
     private static Boolean betaApp;
     public static boolean isBetaApp() {
         if (betaApp == null) {
-            betaApp = ApplicationLoader.applicationContext != null && "org.telegram.messenger.beta".equals(ApplicationLoader.applicationContext.getPackageName());
+            betaApp = ApplicationLoader.applicationContext != null && "top.qwq233.nullgram.beta".equals(ApplicationLoader.applicationContext.getPackageName());
         }
         return betaApp;
     }
---
+++ TMessagesProj/src/main/java/top/qwq2333/nullgram/NullgramPushService.kt
@@ -0,0 +1,36 @@
+/*
+ * Copyright (C) 2019-2022 qwq233 <qwq233@qwq2333.top>
+ * https://github.com/qwq233/Nullgram
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this software.
+ *  If not, see
+ * <https://www.gnu.org/licenses/>
+ */
+
+package top.qwq2333.nullgram
+
+import android.annotation.SuppressLint
+import android.service.notification.NotificationListenerService
+import org.telegram.messenger.ApplicationLoader
+import org.telegram.messenger.KeepAliveJob
+
+@SuppressLint("OverrideAbstract")
+class NekoXPushService : NotificationListenerService() {
+
+    override fun onCreate() {
+        super.onCreate()
+        ApplicationLoader.postInitApplication()
+        KeepAliveJob.startJob()
+    }
+
+}
