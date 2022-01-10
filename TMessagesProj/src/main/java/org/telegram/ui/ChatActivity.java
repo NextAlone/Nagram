6ccf401f8dc243267d481ba32e5d4d3383c56d4a
Nikita S <nikita@saraeff.net>
2022-01-10 16:59:47 +0100

    medias w/ timer in private chats Bug when user sent you a photo with a timer in a private (non-green) chat. You can save these medias without problems.

--- TMessagesProj/src/main/java/org/telegram/ui/ChatActivity.java
+++ TMessagesProj/src/main/java/org/telegram/ui/ChatActivity.java
@@ -12620,6 +12620,7 @@ public class ChatActivity extends BaseFragment implements NotificationCenter.Not
                             File f = FileLoader.getPathToMessage(messageObject.messageOwner);
                             if (f.exists()) {
                                 canSave = true;
+                                sendSecretMessageRead(messageObject, true);
                             }
                         }
                         if (canSave) {
