a9ae0734ad67483eb4ab81e8b44c27627807554e
Nikita S <nikita@saraeff.net>
2022-01-10 16:40:52 +0100

    added feature to stay in chats while banned * ChatActivity.java do not destroy chat frame, but show popup with error (why have no access) * MessagesController.java here we allow to delete chats in DB only if we are decided to delete them, thats why method was changed * ChatObject.java   only `null` or `left==true` will return success for `isNotInChat`. `null` - thats simple, but `left` is when you left a group by yourself and deleted it. w/o `left` it will have ugly bug when you have chat UI active can type and send messages but messages are all have errors due you're not in chat. * MessagesController.java   here is simple, load all the messages.

--- TMessagesProj/src/main/java/org/telegram/ui/ChatActivity.java
+++ TMessagesProj/src/main/java/org/telegram/ui/ChatActivity.java
@@ -18608,7 +18608,7 @@ public class ChatActivity extends BaseFragment implements NotificationCenter.Not
                 removeSelfFromStack();
                 fragment.finishFragment();
             } else {
-                finishFragment();
+//                finishFragment();
             }
         }
     }
