ae1bc3d259945ade478a4f41d286af8e2c3665d0
Nikita S <nikita@saraeff.net>
2022-01-13 13:57:23 +0100

    disabled official update autocheck for the app

--- TMessagesProj/src/main/java/org/telegram/ui/LaunchActivity.java
+++ TMessagesProj/src/main/java/org/telegram/ui/LaunchActivity.java
@@ -3688,6 +3688,7 @@ public class LaunchActivity extends Activity implements ActionBarLayout.ActionBa
     }
 
     public void checkAppUpdate(boolean force) {
+        if (true) return;
         if (!force && BuildVars.DEBUG_VERSION || !force && !BuildVars.CHECK_UPDATES) {
             return;
         }
