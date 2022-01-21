e35034dc6b557796c699ef6dfa20a04d7f13573b
NextAlone <12210746+NextAlone@users.noreply.github.com>
2022-01-22 04:15:57 +0800

    feat: force copy

---
+++ TMessagesProj/src/main/kotlin/xyz/nextalone/nagram/NaConfig.kt
@@ -0,0 +1,162 @@
+package xyz.nextalone.nagram
+
+import android.content.Context
+import android.content.SharedPreferences
+import android.util.Base64
+import org.telegram.messenger.ApplicationLoader
+import tw.nekomimi.nekogram.config.ConfigItem
+import java.io.ByteArrayInputStream
+import java.io.ObjectInputStream
+
+
+object NaConfig {
+    private const val TAG =
+        "NextAlone"
+    val preferences: SharedPreferences =
+        ApplicationLoader.applicationContext.getSharedPreferences(
+            "nkmrcfg",
+            Context.MODE_PRIVATE
+        )
+    val sync =
+        Any()
+    private var configLoaded =
+        false
+    private val configs =
+        ArrayList<ConfigItem>()
+
+    // Configs
+    var forceCopy =
+        addConfig(
+            "NaConfigForceCopy",
+            ConfigItem.configTypeBool,
+            false
+        )
+
+    fun addConfig(
+        k: String,
+        t: Int,
+        d: Any?
+    ): ConfigItem {
+        val a =
+            ConfigItem(
+                k,
+                t,
+                d
+            )
+        configs.add(
+            a
+        )
+        return a
+    }
+
+    fun loadConfig(
+        force: Boolean
+    ) {
+        synchronized(
+            sync
+        ) {
+            if (configLoaded && !force) {
+                return
+            }
+            for (i in configs.indices) {
+                val o =
+                    configs[i]
+                if (o.type == ConfigItem.configTypeBool) {
+                    o.value =
+                        preferences.getBoolean(
+                            o.key,
+                            o.defaultValue as Boolean
+                        )
+                }
+                if (o.type == ConfigItem.configTypeInt) {
+                    o.value =
+                        preferences.getInt(
+                            o.key,
+                            o.defaultValue as Int
+                        )
+                }
+                if (o.type == ConfigItem.configTypeLong) {
+                    o.value =
+                        preferences.getLong(
+                            o.key,
+                            (o.defaultValue as Long)
+                        )
+                }
+                if (o.type == ConfigItem.configTypeFloat) {
+                    o.value =
+                        preferences.getFloat(
+                            o.key,
+                            (o.defaultValue as Float)
+                        )
+                }
+                if (o.type == ConfigItem.configTypeString) {
+                    o.value =
+                        preferences.getString(
+                            o.key,
+                            o.defaultValue as String
+                        )
+                }
+                if (o.type == ConfigItem.configTypeSetInt) {
+                    val ss =
+                        preferences.getStringSet(
+                            o.key,
+                            HashSet()
+                        )
+                    val si =
+                        HashSet<Int>()
+                    for (s in ss!!) {
+                        si.add(
+                            s.toInt()
+                        )
+                    }
+                    o.value =
+                        si
+                }
+                if (o.type == ConfigItem.configTypeMapIntInt) {
+                    val cv =
+                        preferences.getString(
+                            o.key,
+                            ""
+                        )
+                    // Log.e("NC", String.format("Getting pref %s val %s", o.key, cv));
+                    if (cv!!.isEmpty()) {
+                        o.value =
+                            HashMap<Int, Int>()
+                    } else {
+                        try {
+                            val data =
+                                Base64.decode(
+                                    cv,
+                                    Base64.DEFAULT
+                                )
+                            val ois =
+                                ObjectInputStream(
+                                    ByteArrayInputStream(
+                                        data
+                                    )
+                                )
+                            o.value =
+                                ois.readObject() as HashMap<Int?, Int?>
+                            if (o.value == null) {
+                                o.value =
+                                    HashMap<Int, Int>()
+                            }
+                            ois.close()
+                        } catch (e: Exception) {
+                            o.value =
+                                HashMap<Int, Int>()
+                        }
+                    }
+                }
+            }
+            configLoaded =
+                true
+        }
+    }
+
+    init {
+        loadConfig(
+            false
+        )
+    }
+}
