2d5d1afad6f098fa1fe7aa8a6168db90fda2a9a4
NextAlone <12210746+NextAlone@users.noreply.github.com>
2022-01-26 01:11:56 +0800

    update: change update channel

--- TMessagesProj/src/main/java/tw/nekomimi/nekogram/InternalUpdater.java
+++ TMessagesProj/src/main/java/tw/nekomimi/nekogram/InternalUpdater.java
@@ -21,10 +21,10 @@ public class InternalUpdater {
 
     static final int UPDATE_METADATA_START_FROM = 0;
     static final int MAX_READ_COUNT = 20;
-    static final long CHANNEL_METADATA_ID = 1359638116;
-    static final String CHANNEL_METADATA_NAME = "nekox_update_metadata";
-    static final long CHANNEL_APKS_ID = 1137038259;
-    static final String CHANNEL_APKS_NAME = "NekoXApks";
+    static final long CHANNEL_METADATA_ID = 1645699549;
+    static final String CHANNEL_METADATA_NAME = "nagram_metadata";
+    static final long CHANNEL_APKS_ID = 1645699549;
+    static final String CHANNEL_APKS_NAME = "nagram_channel";
 
     static void retrieveUpdateMetadata(retrieveUpdateMetadataCallback callback) {
         final int localVersionCode = BuildVars.BUILD_VERSION;
