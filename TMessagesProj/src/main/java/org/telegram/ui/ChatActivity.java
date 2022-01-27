aca2649a3b96f56ffa88501bd824998c090c968e
NextAlone <12210746+NextAlone@users.noreply.github.com>
2022-01-27 13:32:35 +0800

    fix: typo

--- TMessagesProj/src/main/java/org/telegram/ui/ChatActivity.java
+++ TMessagesProj/src/main/java/org/telegram/ui/ChatActivity.java
@@ -339,8 +339,8 @@ public class ChatActivity extends BaseFragment implements NotificationCenter.Not
     private final static int nkbtn_PGPDecrypt = 2022;
     private final static int nkbtn_PGPImportPrivate = 2023;
     private final static int nkbtn_PGPImport = 2024;
-    private final static int nkbtm_invertReply = 2025;
-    private final static int nkbtm_greatOrPoor = 2026;
+    private final static int nkbtn_invertReply = 2025;
+    private final static int nkbtn_greatOrPoor = 2026;
     
 
     protected TLRPC.Chat currentChat;
@@ -21962,12 +21962,12 @@ public class ChatActivity extends BaseFragment implements NotificationCenter.Not
                             }
                             if (NaConfig.INSTANCE.getShowInvertReply().Bool()) {
                                 items.add(LocaleController.getString("InvertReply", R.string.InvertReply));
-                                options.add(nkbtm_invertReply);
+                                options.add(nkbtn_invertReply);
                                 icons.add(R.drawable.msg_reset);
                             }
                             if (NaConfig.INSTANCE.getShowGreatOrPoor().Bool()) {
                                 items.add(LocaleController.getString("Great", R.string.Great));
-                                options.add(nkbtm_greatOrPoor);
+                                options.add(nkbtn_greatOrPoor);
                                 icons.add(R.drawable.msg_prpr);
                             }
                         }
@@ -23984,11 +23984,11 @@ public class ChatActivity extends BaseFragment implements NotificationCenter.Not
                 repeatMessage(true);
                 return 2;
             }
-            case nkbtm_invertReply: {
+            case nkbtn_invertReply: {
                 invertReplyMessage(true);
                 return 2;
             }
-            case nkbtm_greatOrPoor: {
+            case nkbtn_greatOrPoor: {
                 sendGreatOrGreat(true);
                 return 2;
             }
@@ -29177,10 +29177,10 @@ public class ChatActivity extends BaseFragment implements NotificationCenter.Not
         } else if (id == nkbtn_repeat) {
             repeatMessage(false);
             clearSelectionMode();
-        } else if (id == nkbtm_invertReply) {
+        } else if (id == nkbtn_invertReply) {
             invertReplyMessage(false);
             clearSelectionMode();
-        } else if (id == nkbtm_greatOrPoor) {
+        } else if (id == nkbtn_greatOrPoor) {
             sendGreatOrGreat(false);
             clearSelectionMode();
         }
@@ -29193,11 +29193,11 @@ public class ChatActivity extends BaseFragment implements NotificationCenter.Not
                 repeatMessage(false);
                 break;
             }
-            case nkbtm_invertReply: {
+            case nkbtn_invertReply: {
                 invertReplyMessage(false);
                 break;
             }
-            case nkbtm_greatOrPoor: {
+            case nkbtn_greatOrPoor: {
                 sendGreatOrGreat(false);
                 break;
             }
