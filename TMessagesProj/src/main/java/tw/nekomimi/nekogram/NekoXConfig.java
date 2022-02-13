cad882308c39a9e6c34ad936ddeaba52b08e13c8
NextAlone <12210746+NextAlone@users.noreply.github.com>
2022-02-14 00:12:41 +0800

    chore: add official chats

--- TMessagesProj/src/main/java/tw/nekomimi/nekogram/NekoXConfig.java
+++ TMessagesProj/src/main/java/tw/nekomimi/nekogram/NekoXConfig.java
@@ -37,16 +37,19 @@ public class NekoXConfig {
     //  public static String FAQ_URL = "https://telegra.ph/NekoX-FAQ-03-31";
     public static String FAQ_URL = "https://github.com/NekoX-Dev/NekoX#faq";
     public static long[] officialChats = {
-            1305127566, // NekoX Updates
-            1151172683, // NekoX Chat
-            1299578049, // NekoX Chat Channel
-            1137038259, // NekoX APKs
+//            1305127566, // NekoX Updates
+//            1151172683, // NekoX Chat
+//            1299578049, // NekoX Chat Channel
+//            1137038259, // NekoX APKs
+            1500637449, // Nagram
+            1645699549, // Nagram Updates
     };
 
     public static long[] developers = {
             896711046, // nekohasekai
             380570774, // Haruhi
             784901712, // NextAlone
+            457896977, // Queally
     };
 
     public static final int TITLE_TYPE_TEXT = 0;
