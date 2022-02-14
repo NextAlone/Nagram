0494a56b01fc6b0d517f4390370c53dc763f83ec
Nikita S <nikita@saraeff.net>
2022-02-14 09:47:26 +0100

    raised forward/delete limits to 1024 forward work natively while delete is not. Thats why delete have a "slicer" for a list with the ids. For folks who ask: "WTF your map value contain list of the lists" well, key can be zero (0), but it in theory can contain the ids (here they check `if (newTaskId != 0)`) so to avoid rewriting values in this map we just add new chunks with ids. If they will restrict native forward in future i will add same slicer too. Limit 1024 just to have it. It can be upto 2 billions, but after your client need to send all these chunks (=requests) and you can get api bans (for 5-10min due use abuse their api). So, when you wanna forward 800 messages OR delete them think that TG servers control your API rates for certain api calls. * bugfix for green chats

--- TMessagesProj/src/main/java/org/telegram/ui/ChatActivity.java
+++ TMessagesProj/src/main/java/org/telegram/ui/ChatActivity.java
@@ -1316,7 +1316,8 @@ public class ChatActivity extends BaseFragment implements NotificationCenter.Not
                         return;
                     }
                     if (messageObject.contentType == 0) {
-                        if (selected && selectedMessagesIds[0].size() + selectedMessagesIds[1].size() >= 100) {
+//                        if (selected && selectedMessagesIds[0].size() + selectedMessagesIds[1].size() >= 100) {
+                        if (selected && selectedMessagesIds[0].size() + selectedMessagesIds[1].size() >= 1024) {
                             limitReached = true;
                         } else {
                             limitReached = false;
@@ -13215,7 +13216,8 @@ public class ChatActivity extends BaseFragment implements NotificationCenter.Not
                     }
                 }
             } else {
-                if (selectedMessagesIds[0].size() + selectedMessagesIds[1].size() >= 100) {
+//                if (selectedMessagesIds[0].size() + selectedMessagesIds[1].size() >= 100) {
+                if (selectedMessagesIds[0].size() + selectedMessagesIds[1].size() >= 1024) {
                     AndroidUtilities.shakeView(selectedMessagesCountTextView, 2, 0);
                     Vibrator vibrator = (Vibrator) ApplicationLoader.applicationContext.getSystemService(Context.VIBRATOR_SERVICE);
                     if (vibrator != null) {
