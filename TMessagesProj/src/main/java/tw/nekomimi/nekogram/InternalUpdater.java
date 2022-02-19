424e7103a2396d09ea2931b090d7f6f0b65ff148
freetux <thefreetux@gmail.com>
2022-02-19 13:01:55 +0330

    change update channel

--- TMessagesProj/src/main/java/tw/nekomimi/nekogram/InternalUpdater.java
+++ TMessagesProj/src/main/java/tw/nekomimi/nekogram/InternalUpdater.java
@@ -21,10 +21,10 @@ public class InternalUpdater {
 
     static final int UPDATE_METADATA_START_FROM = 0;
     static final int MAX_READ_COUNT = 20;
-    static final long CHANNEL_METADATA_ID = 1359638116;
-    static final String CHANNEL_METADATA_NAME = "nekox_update_metadata";
-    static final long CHANNEL_APKS_ID = 1137038259;
-    static final String CHANNEL_APKS_NAME = "NekoXApks";
+    static final long CHANNEL_METADATA_ID = 1309937983;
+    static final String CHANNEL_METADATA_NAME = "teletux_update_metadata";
+    static final long CHANNEL_APKS_ID = 1336955033;
+    static final String CHANNEL_APKS_NAME = "TeleTuxapks";
 
     static void retrieveUpdateMetadata(retrieveUpdateMetadataCallback callback) {
         final int localVersionCode = BuildVars.BUILD_VERSION;
