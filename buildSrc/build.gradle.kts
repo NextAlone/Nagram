9729b9636bba74327f420c126e43fd4b5907ce82
qwq233 <qwq233@qwq2333.top>
2022-02-19 14:20:44 +0800

    feat: replace get timestamp as versionCode with commit count
    Signed-off-by: qwq233 <qwq233@qwq2333.top>

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -7,4 +7,19 @@ repositories {
     google()
     gradlePluginPortal()
     mavenCentral()
+
+    dependencies {
+        implementation("org.eclipse.jgit:org.eclipse.jgit:5.10.0.202012080955-r")
+    }
+}
+
+java {
+    targetCompatibility = JavaVersion.VERSION_11
+    sourceCompatibility = JavaVersion.VERSION_11
+}
+
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+    kotlinOptions {
+        jvmTarget = "11"
+    }
 }
--- buildSrc/src/main/kotlin/Common.kt
+++ buildSrc/src/main/kotlin/Common.kt
@@ -1,7 +1,7 @@
+import org.eclipse.jgit.api.Git
+import org.eclipse.jgit.internal.storage.file.FileRepository
 import org.gradle.api.Project
 import java.io.File
-import java.text.SimpleDateFormat
-import java.util.*
 
 object Common {
 
@@ -31,10 +31,37 @@ object Common {
     }
 
     @JvmStatic
-    fun getTimestamp(): Int {
-        val currentTimestamp = (System.currentTimeMillis() / 1000L).toInt()
-        println("currentTimestamp: $currentTimestamp")
-        return currentTimestamp
+    fun getBuildVersionCode(project: Project): Int {
+        // .git/HEAD描述当前目录所指向的分支信息，内容示例："ref: refs/heads/master\n"
+        val headFile = File(project.rootProject.projectDir, ".git" + File.separator + "HEAD")
+        if (headFile.exists()) {
+            var commitHash: String
+            val strings = headFile.readText(Charsets.UTF_8).split(" ")
+            if (strings.size > 1) {
+                val refFilePath = ".git" + File.separator + strings[1];
+                // 根据HEAD读取当前指向的hash值，路径示例为：".git/refs/heads/master"
+                val refFile = File(project.rootProject.projectDir, refFilePath.replace("\n", "").replace("\r", ""));
+                // 索引文件内容为hash值+"\n"，
+                commitHash = refFile.readText(Charsets.UTF_8)
+            } else {
+                commitHash = strings[0]
+            }
+            commitHash = commitHash.trim()
+            val repo = FileRepository(project.rootProject.file(".git"))
+            val refId = repo.resolve(commitHash)
+            val iterator = Git(repo).log().add(refId).call().iterator()
+            var commitCount = 0
+            while (iterator.hasNext()) {
+                commitCount++
+                iterator.next()
+            }
+            repo.close()
+            println("commit Count: $commitCount")
+            return 1645200000 + commitCount
+        } else {
+            println("WARN: .git/HEAD does NOT exist")
+            return 1
+        }
     }
 
 
