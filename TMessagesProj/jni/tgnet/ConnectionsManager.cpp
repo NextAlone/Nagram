fcd10192f0870115fa294d76ba8bedd8f65fbc3c
Ivan K <attoscape@gmail.com>
2022-03-13 21:05:04 +0300

    Increase max accounts to 8

--- TMessagesProj/jni/tgnet/ConnectionsManager.cpp
+++ TMessagesProj/jni/tgnet/ConnectionsManager.cpp
@@ -143,9 +143,24 @@ ConnectionsManager& ConnectionsManager::getInstance(int32_t instanceNum) {
             static ConnectionsManager instance1(1);
             return instance1;
         case 2:
-        default:
             static ConnectionsManager instance2(2);
             return instance2;
+        case 3:
+            static ConnectionsManager instance2(2);
+            return instance3;
+        case 4:
+            static ConnectionsManager instance2(2);
+            return instance4;
+        case 5:
+            static ConnectionsManager instance2(2);
+            return instance5;
+        case 6:
+            static ConnectionsManager instance2(2);
+            return instance6;
+        case 7:
+        default:
+            static ConnectionsManager instance2(2);
+            return instance7;
     }
 }
 
