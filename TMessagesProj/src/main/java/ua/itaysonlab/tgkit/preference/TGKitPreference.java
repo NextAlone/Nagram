44e1786b08cc48244cf7bcb7a48cbd866f25ee37
Ivan K <attoscape@gmail.com>
2022-03-16 22:47:44 +0300

    Nuke TGKit

--- TMessagesProj/src/main/java/ua/itaysonlab/tgkit/preference/TGKitPreference.java
+++
@@ -1,9 +0,0 @@
-package ua.itaysonlab.tgkit.preference;
-
-import ua.itaysonlab.tgkit.preference.types.TGPType;
-
-abstract public class TGKitPreference {
-    public String title;
-
-    abstract public TGPType getType();
-}
