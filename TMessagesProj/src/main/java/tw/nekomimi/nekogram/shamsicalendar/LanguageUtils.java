44041b54df3dd29597bd457a49f0f0e73adc6e3d
luvletter2333 <luvletter2333@gmail.com>
2022-03-27 22:21:19 +0800

    try to fix persion date

--- TMessagesProj/src/main/java/tw/nekomimi/nekogram/shamsicalendar/LanguageUtils.java
+++ TMessagesProj/src/main/java/tw/nekomimi/nekogram/shamsicalendar/LanguageUtils.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 
 public class LanguageUtils {
     public static String getPersianNumbers(String string) {
-        return string.replace("0", "۰").replace("1", "١").replace("2", "۲").replace("3", "۳").replace("4", "۴").replace("5", "۵").replace("6", "۶").replace("7", "۷").replace("8", "۸").replace("9", "۹");
+        return string.replace("0", "۰").replace("1", "١").replace("2", "۲").replace("3", "۳").replace("4", "۴").replace("5", "۵").replace("6", "۶").replace("7", "۷").replace("8", "۸").replace("9", "۹").replace(":", ":");
     }
 
     public static String[] getPersianNumbers(String[] strings) {
