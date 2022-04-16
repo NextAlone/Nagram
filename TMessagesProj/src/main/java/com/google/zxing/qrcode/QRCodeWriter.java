0406db753a8589a081451152e180505368604e4a
NextAlone <12210746+NextAlone@users.noreply.github.com>
2022-04-16 22:59:34 +0800

    fix: wrong icon in QRCode login

--- TMessagesProj/src/main/java/com/google/zxing/qrcode/QRCodeWriter.java
+++ TMessagesProj/src/main/java/com/google/zxing/qrcode/QRCodeWriter.java
@@ -226,7 +226,7 @@ public final class QRCodeWriter {
             canvas.drawBitmap(icon, imageX, imageX, null);
             icon.recycle();
         } else {
-            Drawable drawable = ApplicationLoader.applicationContext.getResources().getDrawable(R.mipmap.ic_launcher);
+            Drawable drawable = ApplicationLoader.applicationContext.getResources().getDrawable(R.mipmap.ic_launcher_nagram);
             drawable.setBounds(imageX, imageX, imageX + imageSize, imageX + imageSize);
             drawable.draw(canvas);
         }
