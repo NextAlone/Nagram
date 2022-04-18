4c3c30a1ff54bcb991ea582ca8b76cca37e17a9d
immat0x1 <immat0x1@protonmail.com>
2022-04-18 13:52:07 +0500

    Improve Message Menu UI

--- TMessagesProj/src/main/java/org/telegram/ui/Components/ReactionsContainerLayout.java
+++ TMessagesProj/src/main/java/org/telegram/ui/Components/ReactionsContainerLayout.java
@@ -280,7 +280,8 @@ public class ReactionsContainerLayout extends FrameLayout implements Notificatio
             lt = (1f - Math.max(CLIP_PROGRESS, transitionProgress));
         }
         rect.set(getPaddingLeft() + (getWidth() - getPaddingRight()) * lt, getPaddingTop() + recyclerListView.getMeasuredHeight() * (1f - otherViewsScale), (getWidth() - getPaddingRight()) * rt, getHeight() - getPaddingBottom());
-        radius = rect.height() / 2f;
+        //radius = rect.height() / 2f;
+        radius = AndroidUtilities.dp(6);
         shadow.setBounds((int) (getPaddingLeft() + (getWidth() - getPaddingRight() + shadowPad.right) * lt - shadowPad.left), getPaddingTop() - shadowPad.top, (int) ((getWidth() - getPaddingRight() + shadowPad.right) * rt), getHeight() - getPaddingBottom() + shadowPad.bottom);
         shadow.draw(canvas);
         canvas.restoreToCount(s);
@@ -342,16 +343,16 @@ public class ReactionsContainerLayout extends FrameLayout implements Notificatio
         canvas.clipRect(0, rect.bottom, getMeasuredWidth(), getMeasuredHeight());
         float cx = LocaleController.isRTL ? bigCircleOffset : getWidth() - bigCircleOffset, cy = getHeight() - getPaddingBottom();
         int sPad = AndroidUtilities.dp(3);
-        shadow.setBounds((int) (cx - br - sPad * cPr), (int) (cy - br - sPad * cPr), (int) (cx + br + sPad * cPr), (int) (cy + br + sPad * cPr));
+        /*shadow.setBounds((int) (cx - br - sPad * cPr), (int) (cy - br - sPad * cPr), (int) (cx + br + sPad * cPr), (int) (cy + br + sPad * cPr));
         shadow.draw(canvas);
-        canvas.drawCircle(cx, cy, br, bgPaint);
+        canvas.drawCircle(cx, cy, br, bgPaint);*/
 
         cx = LocaleController.isRTL ? bigCircleOffset - bigCircleRadius : getWidth() - bigCircleOffset + bigCircleRadius;
         cy = getHeight() - smallCircleRadius - sPad;
         sPad = -AndroidUtilities.dp(1);
-        shadow.setBounds((int) (cx - br - sPad * cPr), (int) (cy - br - sPad * cPr), (int) (cx + br + sPad * cPr), (int) (cy + br + sPad * cPr));
+        /*shadow.setBounds((int) (cx - br - sPad * cPr), (int) (cy - br - sPad * cPr), (int) (cx + br + sPad * cPr), (int) (cy + br + sPad * cPr));
         shadow.draw(canvas);
-        canvas.drawCircle(cx, cy, sr, bgPaint);
+        canvas.drawCircle(cx, cy, sr, bgPaint);*/
         canvas.restore();
     }
 
