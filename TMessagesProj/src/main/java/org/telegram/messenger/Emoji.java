4de017b85ab31b1ef79f54023c91c0c08af24b3f
xtaodada <xtao@xtaolink.cn>
2022-07-08 10:37:11 +0800

    fix: useSystemEmoji

--- TMessagesProj/src/main/java/org/telegram/messenger/Emoji.java
+++ TMessagesProj/src/main/java/org/telegram/messenger/Emoji.java
@@ -545,8 +545,8 @@ public class Emoji {
 
     public static CharSequence replaceEmoji(CharSequence cs, Paint.FontMetricsInt fontMetrics, int size, boolean createNew, int[] emojiOnly, boolean allowAnimated, AtomicReference<WeakReference<View>> viewRef) {
         allowAnimated = false;
-        if (NekoConfig.useSystemEmoji.Bool() || cs == null || cs.length() == 0) {
-            return cs;
+        if (NekoConfig.useSystemEmoji.Bool() || cs.length() == 0) {
+            return Spannable.Factory.getInstance().newSpannable(cs.toString());
         }
         Spannable s;
         if (!createNew && cs instanceof Spannable) {
