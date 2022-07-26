d52e6c8bcbee0638526fd5fe5d5447417b5e3a72
immat0x1 <immat0x1@protonmail.com>
2022-07-27 02:42:32 +0500

    [app] remove β from beta name

--- TMessagesProj/src/main/java/com/exteragram/messenger/preferences/cells/InfoSettingsCell.java
+++ TMessagesProj/src/main/java/com/exteragram/messenger/preferences/cells/InfoSettingsCell.java
@@ -38,7 +38,7 @@ public class InfoSettingsCell extends FrameLayout {
 
         textView = new TextView(context);
         textView.setTextColor(Theme.getColor(Theme.key_windowBackgroundWhiteBlackText));
-        textView.setTypeface(AndroidUtilities.getTypeface("fonts/rregular.ttf"));
+        textView.setTypeface(AndroidUtilities.getTypeface("fonts/rmedium.ttf"));
         textView.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 18);
         textView.setText(ExteraUtils.getAppName() + " | v" + BuildVars.BUILD_VERSION_STRING);
         textView.setLines(1);
