b4080a0a25e6dd550d87cd6d097878cd33401771
xtaodada <xtao@xtaolink.cn>
2024-01-09 00:54:32 +0800

    feat: Added buttons for backward and forward steps to audio player
    Co-authored-by: 23rd <23rd@vivaldi.net>

--- TMessagesProj/src/main/java/org/telegram/messenger/MediaController.java
+++ TMessagesProj/src/main/java/org/telegram/messenger/MediaController.java
@@ -2283,6 +2283,12 @@ public class MediaController implements AudioManager.OnAudioFocusChangeListener,
         return true;
     }
 
+    public void seekShift(int ms) {
+        if (audioPlayer != null) {
+            audioPlayer.seekTo(Math.max(0, audioPlayer.getCurrentPosition() + ms));
+        }
+    }
+
     public long getDuration() {
         if (audioPlayer == null) {
             return 0;
