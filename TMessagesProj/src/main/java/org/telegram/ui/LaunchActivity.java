d5c2a21e0cc9bbb28e83ff21527fcf0cde725c1c
nikita.artikhovich <nikita.artikhovich@gmail.com>
2024-01-20 23:01:41 +0300

    fix memory leak in LaunchActivity

--- TMessagesProj/src/main/java/org/telegram/ui/LaunchActivity.java
+++ TMessagesProj/src/main/java/org/telegram/ui/LaunchActivity.java
@@ -6046,6 +6046,7 @@ public class LaunchActivity extends BasePermissionsActivity implements INavigati
         } catch (Exception e) {
             FileLog.e(e);
         }
+        clearFragments();
         super.onDestroy();
         onFinish();
         FloatingDebugController.onDestroy();
