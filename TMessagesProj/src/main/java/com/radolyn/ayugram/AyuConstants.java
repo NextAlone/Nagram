1fc96153bdfdeef9c04f28c821ecba3795eacc41
cinitdev <setcurl@gamil.com>
2024-12-19 21:28:07 +0800

    Changes\n \n - add BasePreferencesActivity.java \n - add HeaderSettingsCell.java \n - fix strings_nekox.xml

--- TMessagesProj/src/main/java/com/radolyn/ayugram/AyuConstants.java
+++ TMessagesProj/src/main/java/com/radolyn/ayugram/AyuConstants.java
@@ -25,32 +25,9 @@ public class AyuConstants {
             1795176335, // @radolyn_services
     };
 
-    public static final int DOCUMENT_TYPE_NONE = 0;
-    public static final int DOCUMENT_TYPE_PHOTO = 1;
-    public static final int DOCUMENT_TYPE_STICKER = 2;
-    public static final int DOCUMENT_TYPE_FILE = 3;
-
-    public static final int OPTION_HISTORY = 1338_01;
-    public static final int OPTION_TTL = 1338_02;
-    public static final int OPTION_READ_UNTIL = 1338_03;
-
-    public static final int DRAWER_TOGGLE_GHOST = 1000;
-    public static final int DRAWER_KILL_APP = 1001;
-
-    public static final int MESSAGE_EDITED_NOTIFICATION = 6968;
     public static final int MESSAGES_DELETED_NOTIFICATION = 6969;
     public static final int AYUSYNC_STATE_CHANGED = 6970;
     public static final int AYUSYNC_LAST_SENT_CHANGED = 6971;
     public static final int AYUSYNC_LAST_RECEIVED_CHANGED = 6972;
     public static final int AYUSYNC_REGISTER_STATUS_CODE_CHANGED = 6973;
-
-    public static String DEFAULT_DELETED_MARK = "🧹";
-
-    public static String AYU_DATABASE = "ayu-data";
-
-    public static String APP_GITHUB = "AyuGram/AyuGram4A";
-    public static String APP_NAME = "AyuGram";
-
-    public static String BUILD_STORE_PACKAGE = "com.android.vending";
-    public static String BUILD_ORIGINAL_PACKAGE = "org.telegram.messenger";
 }
