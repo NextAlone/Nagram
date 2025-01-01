6244db7b6b49a35f9479dea70b4b71cf80323d6f
cinitdev <setcurl@gamil.com>
2025-01-01 16:04:35 +0800

    log test

--- TMessagesProj/src/main/java/org/telegram/tgnet/ConnectionsManager.java
+++ TMessagesProj/src/main/java/org/telegram/tgnet/ConnectionsManager.java
@@ -438,10 +438,24 @@ SharedPreferences mainPreferences;
 
                             // 设置允许发送已读包
                             AyuState.setAllowReadPacket(true, 1);
-                            sendRequest(request, (a1, a2) -> {
-                                // 调试日志：确认已读请求是否发送
-                                Log.d("ghost","Read history request sent for peer: " + finalPeer + ", MaxId: " + maxId);
+                            TLRPC.TL_updates_getState stateRequest = new TLRPC.TL_updates_getState();
+                            sendRequest(stateRequest, (response, error) -> {
+                                if (response instanceof TLRPC.TL_updates_state) {
+                                    TLRPC.TL_updates_state state = (TLRPC.TL_updates_state) response;
+                                    int newMaxId = state.pts; // 获取最新的 PTS
+                                    // 使用最新的 PTS 来标记已读
+                                    TLRPC.TL_messages_readHistory readHistoryRequest = new TLRPC.TL_messages_readHistory();
+                                    readHistoryRequest.peer = finalPeer;
+                                    readHistoryRequest.max_id = newMaxId;
+                                    AyuState.setAllowReadPacket(true, 1);
+                                    sendRequest(readHistoryRequest, (a1, a2) -> {
+                                        Log.d("ghost", "Read history request sent with MaxId: " + newMaxId);
+                                    });
+                                } else {
+                                    Log.e("ghost", "Failed to fetch state. Error: " + error);
+                                }
                             });
+
                         });
                     };
                 }
