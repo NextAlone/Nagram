a56354d053a10b69ab1c34daf1ebed8d7f785db7
cinitdev <setcurl@gamil.com>
2025-01-05 12:23:23 +0800

    Changelog: • Fix: Long press input state not sending to open ghost mode entry (requires enabling the feature before long pressing). • Enable: Contact birthday reminders.

--- TMessagesProj/src/main/java/tw/nekomimi/nekogram/settings/NekoExperimentalSettingsActivity.java
+++ TMessagesProj/src/main/java/tw/nekomimi/nekogram/settings/NekoExperimentalSettingsActivity.java
@@ -347,6 +347,7 @@ public class NekoExperimentalSettingsActivity extends BaseNekoXSettingsActivity 
                     presentFragment(new NekoGhostModeActivity());
                     return true;
                 }
+
                 createLongClickDialog(context, NekoExperimentalSettingsActivity.this, "experimental", position);
                 return true;
             }
