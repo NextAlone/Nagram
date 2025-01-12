0e8e62d8884825537f69f486c2d6625ab5780b49
任天天 <61044187+ruattd@users.noreply.github.com>
2025-01-12 15:32:38 +0800

    fix: local build on macOS or other platform

--- TMessagesProj/jni/build_ffmpeg_clang.sh
+++ TMessagesProj/jni/build_ffmpeg_clang.sh
@@ -161,7 +161,7 @@ checkPreRequisites
 cd ffmpeg
 
 ## common
-LLVM_PREFIX="${NDK}/toolchains/llvm/prebuilt/linux-x86_64"
+LLVM_PREFIX="${NDK}/toolchains/llvm/prebuilt/${BUILD_PLATFORM}"
 LLVM_BIN="${LLVM_PREFIX}/bin"
 PREFIX_D=$(realpath ..)
 VERSION="4.9"
