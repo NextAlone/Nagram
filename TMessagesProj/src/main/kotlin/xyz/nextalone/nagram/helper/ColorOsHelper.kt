c5165707517229a8c03b13a7129e074ae5b7b268
xtaodada <xtao@xtao.de>
2025-04-16 13:40:58 +0800

    fix: check ColorOS function

--- TMessagesProj/src/main/kotlin/xyz/nextalone/nagram/helper/ColorOsHelper.kt
+++ TMessagesProj/src/main/kotlin/xyz/nextalone/nagram/helper/ColorOsHelper.kt
@@ -1,8 +1,8 @@
 package xyz.nextalone.nagram.helper
 
+import android.text.TextUtils
 import org.telegram.messenger.AndroidUtilities
 
 object ColorOsHelper {
-    val isColorOS: Boolean =
-        AndroidUtilities.getSystemProperty("ro.build.version.oplusrom") != null
+    val isColorOS: Boolean = !TextUtils.isEmpty(AndroidUtilities.getSystemProperty("ro.build.version.oplusrom"))
 }
