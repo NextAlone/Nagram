6eb147f806efdaca7b3387b6b65fffa856274048
Parsast <parsastdev@duck.com>
2026-08-09 20:24:31 +0330

    fix: tg://user open profile
    thanks to @forkgram
    
    Co-authored-by: 23rd <23rd@vivaldi.net>

--- TMessagesProj/src/main/java/org/telegram/ui/LaunchActivity.java
+++ TMessagesProj/src/main/java/org/telegram/ui/LaunchActivity.java
@@ -1583,6 +1583,20 @@ public class LaunchActivity extends BasePermissionsActivity implements INavigati
         return handleIntent(intent, isNew, restore, fromPassword, null, true, false);
     }
 
+    private TLRPC.User resolveLinkUserOrShowError(int account, long userId) {
+        TLRPC.User user = MessagesController.getInstance(account).getUser(userId);
+        if (user == null) {
+            user = MessagesStorage.getInstance(account).getUserSync(userId);
+            if (user != null) {
+                MessagesController.getInstance(account).putUser(user, true);
+            }
+        }
+        if (user == null) {
+            BulletinFactory.global().createErrorBulletin(LocaleController.getString(R.string.NoUsernameFound2)).show();
+        }
+        return user;
+    }
+
     @SuppressLint("Range")
     private boolean handleIntent(Intent intent, boolean isNew, boolean restore, boolean fromPassword, Browser.Progress progress, boolean rebuildFragments, boolean openedTelegram) {
         if (GiftInfoBottomSheet.handleIntent(intent, progress)) {
@@ -1637,6 +1651,7 @@ public class LaunchActivity extends BasePermissionsActivity implements INavigati
         int open_widget_edit = -1;
         int open_widget_edit_type = -1;
         int open_new_dialog = 0;
+        long open_user_id = 0;
         long dialogId = 0;
         boolean showDialogsList = false;
         boolean showPlayer = false;
@@ -2736,16 +2751,12 @@ public class LaunchActivity extends BasePermissionsActivity implements INavigati
                                             open_settings = 1;
                                         }
                                     } else if (url.startsWith("tg:user") || url.startsWith("tg://user")) {
+                                        url = url.replace("tg:user", "tg://telegram.org").replace("tg://user", "tg://telegram.org");
+                                        data = Uri.parse(url);
+
                                         try {
-                                            url = url.replace("tg:user", "tg://telegram.org").replace("tg://user", "tg://telegram.org");
-                                            data = Uri.parse(url);
-                                            long userId = Utilities.parseLong(data.getQueryParameter("id"));
-                                            if (userId != 0) {
-                                                push_user_id = userId;
-                                            }
-                                        } catch (Exception e) {
-                                            FileLog.e(e);
-                                        }
+                                            open_user_id = Long.parseLong(data.getQueryParameter("id"));
+                                        } catch (NumberFormatException ignore) {}
                                     } else if (url.startsWith("tg:upgrade") || url.startsWith("tg://upgrade") || url.startsWith("tg:update") || url.startsWith("tg://update")) {
                                         boolean updateAlways = false;
                                         try {
@@ -3133,7 +3144,7 @@ public class LaunchActivity extends BasePermissionsActivity implements INavigati
                     } else {
                         VoIPPendingCall.startOrSchedule(this, push_user_id, videoCallUser, AccountInstance.getInstance(intentAccount[0]));
                     }
-                } else {
+                } else if (resolveLinkUserOrShowError(intentAccount[0], push_user_id) != null) {
                     Bundle args = new Bundle();
                     args.putLong("user_id", push_user_id);
                     if (push_msg_id != 0) {
@@ -3142,6 +3153,23 @@ public class LaunchActivity extends BasePermissionsActivity implements INavigati
                     if (mainFragmentsStack.isEmpty() || MessagesController.getInstance(intentAccount[0]).checkCanOpenChat(args, mainFragmentsStack.get(mainFragmentsStack.size() - 1))) {
                         ChatActivity fragment = new ChatActivity(args);
                         if (getActionBarLayout().presentFragment(new INavigationLayout.NavigationParams(fragment).setNoAnimation(true))) {
+                            pushOpened = true;
+                            LaunchActivity.dismissAllWeb();
+                        }
+                    }
+                }
+            } else if (open_user_id != 0) {
+                TLRPC.User user = resolveLinkUserOrShowError(intentAccount[0], open_user_id);
+                if (user != null) {
+                    BaseFragment lastFragment = actionBarLayout.getLastFragment();
+                    if (lastFragment != null) {
+                        MessagesController.getInstance(intentAccount[0]).openChatOrProfileWith(user, null, lastFragment, 0, false);
+                        pushOpened = true;
+                        LaunchActivity.dismissAllWeb();
+                    } else {
+                        Bundle args = new Bundle();
+                        args.putLong("user_id", open_user_id);
+                        if (getActionBarLayout().presentFragment(new INavigationLayout.NavigationParams(new ProfileActivity(args)).setNoAnimation(true))) {
                             pushOpened = true;
                             LaunchActivity.dismissAllWeb();
                         }
